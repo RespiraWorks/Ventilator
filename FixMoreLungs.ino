ee3fdad3431c36a865f9b6ab0945c659dcac8949
Edwin Chiu <edwin@soeffects.com>
2020-03-19 13:31:56 -0700

    update comments

--- FixMoreLungs.ino
+++ FixMoreLungs.ino
@@ -1,24 +1,27 @@
 /*
-  Analog input, analog output, serial output
+  Basic test and demo software for COVID-19/ARDS Ventilator
 
-  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
-  the result to set the pulse width modulation (PWM) of an output pin.
-  Also prints the results to the Serial Monitor.
+  Objective is to make a minimum-viable ARDS ventilator that can be deployed 
+  or constructed on-site in countries underserved by commecial global supply 
+  chains during the COVID-19 outbreak.
+  
+  Currently consists of a (1) CPAP-style blower with speed control.
+  
+  (2)Feedback from a differential pressure sensor with one side
+  measuring delivered pressure to patient, other side ambient.
+  
 
-  The circuit:
-  - potentiometer connected to analog pin 0.
-    Center pin of the potentiometer goes to the analog pin.
-    side pins of the potentiometer go to +5V and ground
-  - LED connected from digital pin 9 to ground
+  created 16 Mar 2020
+  Edwin Chiu
+  Frost Methane Labs/Fix More Lungs
+  Based on example code by Tom Igoe and Brett Beauregard
 
-  created 29 Dec. 2008
-  modified 9 Apr 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
+  Project Description: http://bit.ly/2wYqj3X
+  git: https://github.com/inceptionev/FixMoreLungs
+  www.pandemicventilator.com
+  
 */
+
 #include <PID_v1.h>
 
 #define BLOWERSPD_PIN 3
@@ -71,10 +74,6 @@ void loop() {
   analogWrite(BLOWERSPD_PIN, Output);
 
   // print the results to the Serial Monitor:
-  //Serial.print("Input = ");
-  //Serial.print(Input);
-  //Serial.print("\t output = ");
-  //Serial.println(Output);
   Serial.print("C");
   Serial.write(int(Setpoint)>>8);
   Serial.write(int(Setpoint)&0xff);

34228062ddafbff35f89b498088ebf7aaaf2e20b
Andrew Davies <awdavies@google.com>
2020-03-22 17:46:49 -0700

    Formatting

--- comms.ino
+++ comms.ino
@@ -19,13 +19,13 @@
 
 void comms_init() {
   // initialize serial communications at 115200 bps
-	Serial.begin(115200);
+  Serial.begin(115200);
 }
 
 void comms_send(double Setpoint, int sensorValue) {
-	Serial.print("C"); //output to monitor
-	Serial.write(int(map(Setpoint,0,255,0,1023))>>8); //output to monitor
-	Serial.write(int(map(Setpoint,0,255,0,1023))&0xff); //output to monitor
-	Serial.write(int(sensorValue)>>8); //output to monitor
-	Serial.write(int(sensorValue)&0xff); //output to monitor
+  Serial.print("C"); //output to monitor
+  Serial.write(int(map(Setpoint, 0, 255, 0, 1023)) >> 8); //output to monitor
+  Serial.write(int(map(Setpoint, 0, 255, 0, 1023)) & 0xff); //output to monitor
+  Serial.write(int(sensorValue) >> 8); //output to monitor
+  Serial.write(int(sensorValue) & 0xff); //output to monitor
 }

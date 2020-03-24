eb4723dbc361620fc0eb636eebfb49adbd2bf8a9
rwilbur <wilburriley@gmail.com>
2020-03-24 12:07:42 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include <QtCharts/QChartGlobal>
+
+
+int main(int argc, char *argv[])
+{
+    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
+
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}

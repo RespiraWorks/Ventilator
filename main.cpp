84225f088dbbe8d5faf2cf092567e834e91a64fc
rwilbur <wilburriley@gmail.com>
2020-03-28 16:09:09 -0400

    Add 3rd graph and settings page layout

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    app.setWindowIcon(QIcon("Logo.png"));
+    app.setWindowIcon(QIcon(":/Logo.png"));
     QQuickView mainView;
 
     QString extraImportPath(QStringLiteral("%1/../../../%2"));
@@ -19,11 +19,12 @@ int main(int argc, char *argv[])
     mainView.setTitle(QStringLiteral("Ventilator"));
 
     DataSource pressureDataSource(&mainView); // This will need to be a source coming from the arduino
-
     DataSource volumeDataSource(&mainView); // This will need to be a source coming from the arduino
+    DataSource flowDataSource(&mainView); // This data source will be calulated from pressure and volume on the pi
 
     mainView.rootContext()->setContextProperty("pressureDataSource", &pressureDataSource);
     mainView.rootContext()->setContextProperty("volumeDataSource", &volumeDataSource);
+    mainView.rootContext()->setContextProperty("flowDataSource", &flowDataSource);
 
     mainView.setSource(QUrl("qrc:/main.qml"));
     mainView.setResizeMode(QQuickView::SizeRootObjectToView);

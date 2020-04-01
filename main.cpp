2e616b4a57f98b4c3ce17d71cbf956cbd3df6d56
Martin Shetty <martukas@gmail.com>
2020-04-01 06:19:39 -0400

    "run and die after build; updates #11"

--- main.cpp
+++ main.cpp
@@ -3,14 +3,29 @@
 #include <QtQuick/QQuickView>
 #include <QtQml/QQmlEngine>
 #include <QtCore/QDir>
+#include <QCommandLineParser>
 #include "datasource.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     app.setWindowIcon(QIcon(":/Logo.png"));
-    QQuickView mainView;
 
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Ventilator GUI application");
+    parser.addHelpOption();
+    //  parser.addVersionOption();
+
+    QCommandLineOption dieOption(QStringList() << "d" << "die",
+                                QApplication::translate("main", "Die right away (for testing)"));
+    parser.addOption(dieOption);
+
+    parser.process(app);
+
+    bool die_now = parser.isSet(dieOption);
+
+
+    QQuickView mainView;
     QString extraImportPath(QStringLiteral("%1/../../../%2"));
 
     mainView.engine()->addImportPath(extraImportPath.arg(QGuiApplication::applicationDirPath(),
@@ -29,7 +44,10 @@ int main(int argc, char *argv[])
     mainView.setSource(QUrl("qrc:/main.qml"));
     mainView.setResizeMode(QQuickView::SizeRootObjectToView);
     mainView.setColor(QColor("#000000"));
-    mainView.show();
 
-    return app.exec();
+    if (!parser.isSet("h") && !die_now)
+    {
+      mainView.show();
+      return app.exec();
+    }
 }

865b9dbda1d651943b9a0bb4e89312be9cfaa3aa
Justin Lebar <jlebar@google.com>
2020-04-13 13:27:49 -0700

    Fix fletcher16 checksum computation.
    In the process of adding the more elaborate tests in this commit, I
    found that fletcher16 was not performing well.  Out of the 2^16 random
    single bit-flips I tried, 25 had the same checksum.  And one particular
    checksum value had ~128/2^16 preimages, i.e. 128 of the 2^16 inputs we
    tried all hashed to the same value.
    
    After a bit of investigation, I realized that this was all due to a
    subtle C typecasting bug:
    
        *sum1 = ((*sum1) + data[index]) % 255;
    
    The issue is that `data` is a `char`, which may be signed or unsigned
    depending on the platform.  If it's signed, then this is bad!
    
    This commit fixes the problem, adds tests that catch the problem
    statistically and explicitly, and also simplifies the code and API.

--- controller/test/checksum/checksum_test.cpp
+++ controller/test/checksum/checksum_test.cpp
@@ -1,26 +1,90 @@
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include "unity.h"
+#include <map>
+
 #include "checksum.h"
+#include "unity.h"
 
-void test_Empty() {
-  uint16_t sum1 = 0;
-  uint16_t sum2 = 0;
-  uint16_t actual = checksum_fletcher16(&sum1, &sum2, /*data=*/NULL, /*count=*/0);
-  TEST_ASSERT_EQUAL_INT16(0, actual);
+void test_KnownQuantities() {
+  TEST_ASSERT_EQUAL_INT16(0, checksum_fletcher16(NULL, 0));
+  TEST_ASSERT_EQUAL_INT16(0, checksum_fletcher16("", 0));
+  TEST_ASSERT_EQUAL_INT16(24929, checksum_fletcher16("a", 1));
+  TEST_ASSERT_EQUAL_INT16(51440, checksum_fletcher16("abcde", 5));
+  TEST_ASSERT_EQUAL_INT16(8279, checksum_fletcher16("abcdef", 6));
+  TEST_ASSERT_EQUAL_INT16(1575, checksum_fletcher16("abcdefgh", 8));
+  TEST_ASSERT_EQUAL_INT16(0xfefe, checksum_fletcher16("\xff\xfe", 2));
 }
 
-void test_OneByte() {
-  uint16_t sum1 = 0;
-  uint16_t sum2 = 0;
-  char data[1] = {'a'};
-  uint16_t actual = checksum_fletcher16(&sum1, &sum2, data, /*count=*/1);
-  TEST_ASSERT_EQUAL_INT16(24929, actual);
+void test_CheckBytes() {
+  uint16_t csum = checksum_fletcher16("abcde", 5);
+
+  uint16_t checkBytes = check_bytes_fletcher16(csum);
+  char c0 = checkBytes >> 8;
+  char c1 = checkBytes & 0xff;
+  csum = checksum_fletcher16(&c0, 1, csum);
+  csum = checksum_fletcher16(&c1, 1, csum);
+  TEST_ASSERT_EQUAL_INT16(csum, 0);
 }
 
-int main () {
-    UNITY_BEGIN();
-    RUN_TEST(test_Empty);
-    RUN_TEST(test_OneByte);
-    return UNITY_END();
+// Repeatedly flip a random bit in `data` and check that
+//  - a one-bit flip never yields the same checksum, and
+//  - the same checksum doesn't appear "too often" overall.
+void test_BitFlips() {
+  srand(0);
+
+  char data[32];
+  memset(&data, '\0', sizeof(data));
+
+  // Start lastChecksum at -1 because our first test will be checksum'ing the
+  // all-zeroes buffer, which should yield 0.
+  uint16_t lastChecksum = -1;
+
+  const int32_t numTests = int32_t{1} << 16;
+  int32_t singleBitFlipCollisions = 0;
+  std::map<int16_t, int32_t> collisions;
+  for (int32_t i = 0; i < numTests; ++i) {
+    uint16_t checksum = checksum_fletcher16(data, sizeof(data));
+    collisions[checksum]++;
+    if (checksum == lastChecksum) {
+      singleBitFlipCollisions++;
+    }
+
+    lastChecksum = checksum;
+    int bitToFlip = rand() % (8 * sizeof(data));
+    uint8_t byteMask = 1 << (bitToFlip % 8);
+    data[bitToFlip / 8] ^= byteMask;
+  }
+
+  printf("%d/%d collisions after flipping a single bit.\n",
+         singleBitFlipCollisions, numTests);
+  if (singleBitFlipCollisions > 0) {
+    TEST_FAIL_MESSAGE("Got at least one collision from one-bit flips; is the "
+                      "checksum broken?\n");
+  }
+
+  // Find the checksum with the most collisions.  There shouldn't be "too
+  // many".
+  using MapElem = decltype(*collisions.begin());
+  auto it = std::max_element(
+      collisions.begin(), collisions.end(),
+      [](MapElem a, MapElem b) { return a.second < b.second; });
+  int16_t maxCollisionHash = it->first;
+  int32_t maxCollisions = it->second;
+  double maxCollisionsFrac = 1.0 * maxCollisions / numTests;
+  printf("%d/%d collisions on worst checksum, 0x%4x\n", maxCollisions, numTests,
+         maxCollisionHash);
+  if (maxCollisionsFrac >= 0.0002) {
+    TEST_FAIL_MESSAGE(
+        "Too many collisions on worst checksum; is the checksum broken?");
+  }
+}
+
+int main() {
+  UNITY_BEGIN();
+  RUN_TEST(test_KnownQuantities);
+  RUN_TEST(test_BitFlips);
+  return UNITY_END();
 }

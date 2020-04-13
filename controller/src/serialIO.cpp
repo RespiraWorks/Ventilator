c59644f991a8321205b02af27685308bf9fd7d8a
Justin Lebar <jlebar@google.com>
2020-04-13 13:27:49 -0700

    Fix fletcher16 checksum computation.
    In the process of adding the more elaborate tests in this commit, I
    found that fletcher16 was not performing well.  Out of the 2^16 random
    single bit-flips I tried, 25 had the same checksum.  And one particular
    checksum value had ~128/2^16 preimages, i.e. 128 of the 2^16 inputs we
    tried all hashed to the same value.
    
    After a bit of investigation, I realized that this was all due to a
    subtle C typecasting bug:
    
        *sum1 = ((*sum1) + data[index]) % 255;
    
    The issue is that `data` is a `char`, which may be signed or unsigned
    depending on the platform.  If it's signed, then this is bad!
    
    This commit fixes the problem, adds tests that catch the problem
    statistically and explicitly, and also simplifies the code and API.

--- controller/src/serialIO.cpp
+++ controller/src/serialIO.cpp
@@ -20,47 +20,34 @@
 #include "serialIO.h"
 
 void serialIO_init() {
-	// initialize serial communications at 115200 bps
-    Serial.begin(115200, SERIAL_8N1);
+  // Initialize serial communications at 115200 bps.
+  Serial.begin(115200, SERIAL_8N1);
 }
 
 bool serialIO_dataAvailable() {
-
-    return (Serial.available() > 0) ? true : false;
+  return (Serial.available() > 0) ? true : false;
 }
 
 void serialIO_readByte(char *buffer) {
-    // NOTE: This assumes that a byte is ready in the buffer
-    Serial.readBytes(buffer, 1);
+  // NOTE: This assumes that a byte is ready in the buffer
+  Serial.readBytes(buffer, 1);
 }
 
 void serialIO_send(enum msgType type, enum dataID id, char *data, uint8_t len) {
+  char metadata[3] = {
+      (char)((char)type & 0xff),
+      (char)id,
+      (char)len,
+  };
 
-    char metadata[3];
-    uint16_t sum1 = 0;
-    uint16_t sum2 = 0;
-    uint16_t csum;
-    uint8_t c0,c1,f0,f1;
+  // Calculate checksum and check bytes.
+  uint16_t csum = checksum_fletcher16(metadata, sizeof(metadata));
+  csum = checksum_fletcher16(data, len, csum);
+  uint16_t check_bytes = check_bytes_fletcher16(csum);
 
-    metadata[0] = ((char) type) & 0xff; // DATA_TYPE
-    metadata[1] = (char)  id; // DATA_ID
-    metadata[2] = (char)  len; // LEN
-
-    checksum_fletcher16(&sum1, &sum2, metadata, sizeof(metadata));
-    csum = checksum_fletcher16(&sum1, &sum2, data, len);
-
-    // Calculate check bytes
-    // TODO Can this be optimised?
-    f0 = csum & 0xff;
-    f1 = (csum >> 8) & 0xff;
-    c0 = 0xff - ((f0 + f1) % 0xff);
-    c1 = 0xff - ((f0 + c0) % 0xff);
-
-    Serial.write(metadata, sizeof(metadata));  // Send DATA_TYPE, DATA_ID, LEN
-    Serial.write(data, len);  // Send DATA
-
-    // Send checksum
-    Serial.write(c0);
-    Serial.write(c1);
-
+  // Send the packet: [DATA_TYPE, DATA_ID, LEN, DATA, check bytes].
+  Serial.write(metadata, sizeof(metadata));
+  Serial.write(data, len);
+  Serial.write(static_cast<uint8_t>(check_bytes >> 8));
+  Serial.write(static_cast<uint8_t>(check_bytes & 0xff));
 }

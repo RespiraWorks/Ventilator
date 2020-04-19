43b636fb9c4103e97cd0cf9ae8e738d5d0063247
miceuz <mic@hardcore.lt>
2020-04-19 10:32:46 +0300

    clean up comments, variable and type names add small fixes related to review - comments, var names and types rename ack_handler, improve command message decoder, fix bugs related to it where message length was incorrectly specified convert comment to TODO apply formatting rules to the proto file add comments explaining Commands VentilatorController handles rename AlertLevel type, also add more comments on usage of different data packets pass objects by reference to command and gui ack handlers. Demote command handlers to comms library Consolidate all serdes code to a single file
    pull ControllerStatus and network-protocol dependency up to pid.c.
    
    migrate tests to gtest

--- controller/test/comms/test_comms.cpp
+++ controller/test/comms/test_comms.cpp
@@ -1,4 +1,4 @@
-#include "unity.h"
+#include "gtest/gtest.h"
 
 #include "comms.h"
 
@@ -7,7 +7,6 @@
 #include <pb_decode.h>
 #include <pb_encode.h>
 
-static uint64_t time = 0x4242;
 
 #define PACKET_LEN_MAX (32)
 static uint8_t tx_buffer[PACKET_LEN_MAX];
@@ -15,11 +14,15 @@ static uint8_t tx_idx = 0;
 static uint8_t rx_buffer[PACKET_LEN_MAX];
 static uint8_t rx_idx = 0;
 static uint8_t rx_data_len = 0;
-uint64_t millis() { return time++; }
+
+uint64_t millis() {
+  static uint64_t time = 0x4242;
+  return time++;
+}
 
 void serialIO_readByte(char *buffer) {
   if (rx_idx >= PACKET_LEN_MAX || rx_idx > rx_data_len) {
-    TEST_FAIL_MESSAGE("rx buffer underflow");
+    FAIL() << "rx buffer underflow";
   }
   *buffer++ = rx_buffer[rx_idx++];
 }
@@ -34,43 +37,49 @@ bool serialIO_dataAvailable() { return rx_idx < rx_data_len; }
 void serialIO_send(uint8_t b) {
   tx_buffer[tx_idx++] = b;
   if (tx_idx >= PACKET_LEN_MAX) {
-    TEST_FAIL_MESSAGE("tx buffer overflow");
+    FAIL() << "tx buffer overflow";
   }
 }
 
-void test_sendPeriodicReadings() {
+TEST(CommTests, SendControllerStatus) {
   float pressure = 1.1;
   float volume = 2.2;
   float flow = 3.3;
   tx_idx = 0;
 
-  comms_sendPeriodicReadings(pressure, volume, flow);
+  ControllerStatus controller_status = ControllerStatus_init_zero;
+  controller_status.time = 100;
+  controller_status.flow = flow;
+  controller_status.volume = volume;
+  controller_status.pressure = pressure;
+
+  comms_sendControllerStatus(controller_status);
   comms_handler();
 
-  TEST_ASSERT_TRUE(tx_idx != 0);
+  ASSERT_NE(tx_idx, 0);
   pb_istream_t stream = pb_istream_from_buffer(tx_buffer, tx_idx);
   Packet packet = Packet_init_zero;
   bool status = pb_decode(&stream, Packet_fields, &packet);
-  TEST_ASSERT_EQUAL_INT16(true, status);
-  TEST_ASSERT_EQUAL_INT16(Packet_data_tag, packet.which_payload);
-  TEST_ASSERT_EQUAL_INT16(ControllerMsgType_DATA, packet.payload.data.msg_type);
-  TEST_ASSERT_EQUAL_INT16(ControllerData_status_tag,
-                          packet.payload.data.which_payload);
+  ASSERT_TRUE(status);
+  ASSERT_EQ(packet.which_payload, Packet_data_tag);
+  ASSERT_EQ(packet.payload.data.msg_type, ControllerMsgType_DATA);
+  ASSERT_EQ(packet.payload.data.which_payload, ControllerData_status_tag);
   ControllerStatus s = packet.payload.data.payload.status;
-  TEST_ASSERT_EQUAL_FLOAT(s.pressure, pressure);
-  TEST_ASSERT_EQUAL_FLOAT(s.volume, volume);
-  TEST_ASSERT_EQUAL_FLOAT(s.flow, flow);
+  ASSERT_FLOAT_EQ(s.pressure, pressure);
+  ASSERT_FLOAT_EQ(s.volume, volume);
+  ASSERT_FLOAT_EQ(s.flow, flow);
 }
 
 bool command_handler_called = false;
-void command_handler(Command cmd) {
+
+void command_handler(const Command &cmd) {
   command_handler_called = true;
-  TEST_ASSERT_TRUE(cmd.has_data);
-  TEST_ASSERT_EQUAL_INT32(CommandType_NONE, cmd.cmd);
-  TEST_ASSERT_EQUAL_FLOAT(42.42, cmd.data);
+  ASSERT_TRUE(cmd.has_data);
+  ASSERT_EQ(cmd.cmd, CommandType_NONE);
+  ASSERT_FLOAT_EQ(cmd.data, 42.42);
 }
 
-void ack_handler(GuiAck ack) {}
+void gui_ack_handler(const GuiAck &ack) {}
 
 void test_command_rx() {
   Command cmd = Command_init_zero;
@@ -87,17 +96,10 @@ void test_command_rx() {
   rx_data_len = stream.bytes_written;
   rx_idx = 0;
 
-  TEST_ASSERT_TRUE(serialIO_dataAvailable());
+  ASSERT_TRUE(serialIO_dataAvailable());
 
   comms_handler();
   comms_handler();
 
-  TEST_ASSERT_TRUE(command_handler_called);
-}
-
-int main() {
-  UNITY_BEGIN();
-  RUN_TEST(test_sendPeriodicReadings);
-  RUN_TEST(test_command_rx);
-  return UNITY_END();
+  ASSERT_TRUE(command_handler_called);
 }

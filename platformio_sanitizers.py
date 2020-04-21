a577dc53614d850c8afeb6d62c282ef5bfdf1c13
miceuz <mic@hardcore.lt>
2020-04-21 17:54:07 +0300

    Disable memory sanitizer for now

--- platformio_sanitizers.py
+++ platformio_sanitizers.py
@@ -8,10 +8,12 @@ sanitizers = [
       "-fsanitize=undefined",
       "-fsanitize=address",
 ]
-if sys.platform == "linux":
-    # https://releases.llvm.org/3.7.0/tools/clang/docs/MemorySanitizer.html#supported-platforms
-    # Currently only works on Linux but e.g. not Mac OS.
-    sanitizers.append('-fsanitize=memory')
+
+#GCC does not have memory option to -fsanitize
+#if sys.platform == "linux":
+#    # https://releases.llvm.org/3.7.0/tools/clang/docs/MemorySanitizer.html#supported-platforms
+#    # Currently only works on Linux but e.g. not Mac OS.
+#    sanitizers.append('-fsanitize=memory')
 
 env.Append(CCFLAGS=sanitizers)
 env.Append(LINKFLAGS=sanitizers)

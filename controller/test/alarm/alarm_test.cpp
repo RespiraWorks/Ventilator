e8eb8293603b656ef9c3d78b120ad65db1034f41
Sarah Adelaide <sarahadelaideevans@gmail.com>
2020-04-24 23:32:14 -0700

    Revert "Merge pull request #141 from RespiraWorks/SarahAdelaide/UnitTests"
    This reverts commit a8ac3c4bcec8410c656bd0d464bdd0a6d99fdb6a, reversing
    changes made to 693044a6f3ea048f140c4f7ea03489e0153d179c.

--- controller/test/alarm/alarm_test.cpp
+++ controller/test/alarm/alarm_test.cpp
@@ -7,51 +7,17 @@ class AlarmTest : public testing::Test {
 public:
   void SetUp() override {
     alarm_init();
-    while (alarm_available()) {
-      alarm_remove();
-    }
     // TODO: Reset Hal mock object.
   }
 };
 
 TEST_F(AlarmTest, NotAvailableAfterInit) { ASSERT_FALSE(alarm_available()); }
 
-TEST_F(AlarmTest, AddAndRemoveOneAlarm) {
+TEST_F(AlarmTest, AddOneAlarm) {
   char alarm_data[ALARM_DATALEN];
   memset(alarm_data, 'x', sizeof(alarm_data));
 
   alarm_add(alarm_data);
-  ASSERT_TRUE(alarm_available());
-  alarm_remove();
-  ASSERT_FALSE(alarm_available());
-}
-
-TEST_F(AlarmTest, OverflowAlarmQueue) {
-  for (uint8_t i = 0; i <= ALARM_NODES; i++) {
-    char alarm_data[ALARM_DATALEN];
-    memset(alarm_data, 'x', sizeof(alarm_data));
-    alarm_add(dataID::alarm_1, alarm_data);
-  }
-  ASSERT_TRUE(alarm_available());
-  // TODO(SarahAdelaide) when logging is implemented, validate log is generated
-}
-
-TEST_F(AlarmTest, AddAndReadAlarm) {
-  char alarm_data_a[ALARM_DATALEN];
-  memset(alarm_data_a, 'y', sizeof(alarm_data_a));
-  dataID alarm_id_a = dataID::alarm_2;
-  alarm_add(alarm_id_a, alarm_data_a);
-  // checking that alarm data got copied, not referenced
-  memset(alarm_data_a, 'x', sizeof(alarm_data_a));
-  alarm_id_a = dataID::alarm_1;
-
-  enum dataID alarm_id_b;
-  uint32_t alarm_timestamp_b;
-  char alarm_data_b[ALARM_DATALEN];
-  int32_t res = alarm_read(&alarm_id_b, &alarm_timestamp_b, alarm_data_b);
-  ASSERT_EQ(res, VC_STATUS_SUCCESS);
-  ASSERT_EQ(alarm_data_b[0], 'y');
-  ASSERT_EQ(alarm_id_b, dataID::alarm_2);
   ASSERT_TRUE(alarm_available());
 }
 
@@ -59,3 +25,6 @@ TEST_F(AlarmTest, Timestamp) {}
 
 // TODO(jlebar): Add more tests, checking that:
 //  - Alarms get dropped after a certain point.
+//  - Alarms can be popped.
+//  - Alarm data gets copied into storage (not just referring to the pointer
+//    passed to alarm_add).

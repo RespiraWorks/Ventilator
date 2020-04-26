7156e22f4d4e5755d6b2ddff87fccc151110b5f0
Eugene Kirpichov <ekirpichov@gmail.com>
2020-04-26 16:12:20 -0700

    Make the rest of the code work with imported PID
     * MIT license
     * Uses HAL
     * Mentions original project hash
     * Removes Arduino.h from main.cpp
     * Imports Arduino map function
     * Makes some variables in control.cpp static

--- controller/lib/pid/pid.h
+++ controller/lib/pid/pid.h
@@ -1,5 +1,23 @@
-#ifndef PID_v1_h
-#define PID_v1_h
+/* Copyright 2020, RespiraWorks
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/**********************************************************************************************
+ * Adapted from Arduino PID Library - Version 1.2.1 under MIT License
+ * by Brett Beauregard <br3ttb@gmail.com> brettbeauregard.com
+ * Original project hash: 9b4ca0e5b6d7bab9c6ac023e249d6af2446d99bb
+ **********************************************************************************************/
+
+#ifndef PID_H
+#define PID_H
 #define LIBRARY_VERSION 1.2.1
 
 class PID {

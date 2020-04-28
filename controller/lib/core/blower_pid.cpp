8ef34d7d3ca153a0743836c7e8314c4696ee6fe7
Justin Lebar <justin.lebar@gmail.com>
2020-04-27 17:13:28 -0700

    New blower FSM.
    The moment we've all been waiting for...
    
    - Create blower_fsm, which separates out controlling the pressure
      setpoint from the PID loop that drives the fan to achieve this
      setpoint.
    
    - Rename control to blower_pid now that its role is smaller.
    
    - Add setpoint field to ControllerStatus proto so the GUI can observe
      the pressure we're trying to drive.
    
    Fixes issue #17.
    
    GPC: blower-fsm

---
+++ controller/lib/core/blower_pid.cpp
@@ -0,0 +1,73 @@
+/* Copyright 2020, Edwin Chiu
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "blower_pid.h"
+
+#include "comms.h"
+#include "hal.h"
+#include "sensors.h"
+#include "types.h"
+
+static double Setpoint;
+static double Input;
+static double Output;
+
+// Configure the PID
+// TODO: Tune these params.
+static constexpr float Kp = 2;
+static constexpr float Ki = 8;
+static constexpr float Kd = 0;
+
+// DIRECT means that increases in the output should result in increases in the
+// input.  DIRECT as opposed to REVERSE.
+static PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+void blower_pid_init() {
+  Setpoint = 0;
+  Input = 0;
+  Output = 0;
+
+  // Turn the PID on.
+  myPID.SetMode(AUTOMATIC);
+
+  // TODO: PID updates at 100ms by default, which is probably too slow for us?
+}
+
+void blower_pid_execute(Pressure setpoint, SensorReadings *readings,
+                        float *fan_power) {
+  Pressure cur_pressure = get_patient_pressure();
+
+  Setpoint = setpoint.kPa();
+  Input = cur_pressure.kPa();
+  myPID.Compute();
+  Hal.analogWrite(PwmPin::BLOWER, static_cast<int>(Output));
+
+  // fan_power is in range [0, 1].
+  *fan_power = stl::min(stl::max(Output, 0.), 255.) / 255.;
+
+  // Store sensor readings so they can eventually be sent to the GUI.
+  // This pressure is just from the patient sensor, converted to the right
+  // units.
+  readings->pressure_cm_h2o = cur_pressure.cmH2O();
+  // TODO(anyone): This value is to be the integration over time of the below
+  // flow_ml_per_min. That is, you take the value calculated for flow_ml_per_min
+  // and integrate it over time to get total volume at the current time. Don't
+  // think this necessarily has to be done on the controller though?
+  readings->volume_ml = 0;
+  // Flow rate is inhalation flow minus exhalation flow. Positive value is flow
+  // into lungs, and negative is flow out of lungs.
+  readings->flow_ml_per_min =
+      (get_volumetric_inflow() - get_volumetric_outflow()).ml_per_min();
+}

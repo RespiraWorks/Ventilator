a248369feface4365d2eb8b5314209d7f2f8663a
asmodai27 <asmodai27@wanadoo.fr>
2020-05-01 23:46:49 +0200

    add D_ON_E mode to allow using derivative on error rather than on measurement (and made it the default) will require adding tests to both D_ON_E and D_ON_M modes

--- controller/lib/pid/pid.h
+++ controller/lib/pid/pid.h
@@ -32,14 +32,16 @@ public:
 #define REVERSE 1
 #define P_ON_M 0
 #define P_ON_E 1
+#define D_ON_M 0
+#define D_ON_E 1
 
   // commonly used functions
   // **************************************************************************
   PID(double *, double *,
       double *, // * constructor.  links the PID to the Input, Output, and
-      double, double, double, int,
+      double, double, double, int, int,
       int); //   Setpoint.  Initial tuning parameters are also set here.
-            //   (overload for specifying proportional mode)
+            //   (overload for specifying proportional and derivative modes)
 
   PID(double *, double *,
       double *, // * constructor.  links the PID to the Input, Output, and
@@ -68,6 +70,9 @@ public:
   void SetTunings(double, double, // * overload for specifying proportional mode
                   double, int);
 
+  void SetTunings(double, double, // * overload for specifying derivative mode
+                  double, int, int);
+
   void SetControllerDirection(
       int); // * Sets the Direction, or "Action" of the controller. DIRECT
             //   means the output will increase when error is positive. REVERSE
@@ -95,7 +100,7 @@ private:
   double kd; // * (D)erivative Tuning Parameter
 
   int controllerDirection;
-  int pOn;
+  int pOn, dOn;
 
   double *myInput;  // * Pointers to the Input, Output, and Setpoint variables
   double *myOutput; //   This creates a hard link between the variables and the
@@ -104,9 +109,9 @@ private:
                    //   what these values are.  with pointers we'll just know.
 
   Duration lastTime, lastSampleTime;
-  double outputSum, lastInput;
+  double outputSum, lastInput, lastError;
 
   double outMin, outMax;
-  bool inAuto, pOnE;
+  bool inAuto, pOnE, dOnE;
 };
 #endif

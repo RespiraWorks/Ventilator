6f3a52a163a6a99d40bfb6bdc51f6fa4a570239c
asmodai27 <asmodai27@wanadoo.fr>
2020-05-03 10:55:49 +0200

    code review from jkff: - clarify API in header file - clarify comments and variable names - clean PonE/POn and DonE/DOn redundancy - clarify PID time management

--- controller/lib/pid/pid.h
+++ controller/lib/pid/pid.h
@@ -37,16 +37,17 @@ public:
 
   // commonly used functions
   // **************************************************************************
-  PID(double *, double *,
-      double *, // * constructor.  links the PID to the Input, Output, and
-      double, double, double, int, int,
-      int); //   Setpoint.  Initial tuning parameters are also set here.
-            //   (overload for specifying proportional and derivative modes)
+  PID(double *Input, double *Output, double *Setpoint, double Kp, double Ki,
+      double Kd, bool POn, bool DOn, int ControllerDirection);
+  // * constructor.  links the PID to the Input, Output, and
+  //   Setpoint.  Initial tuning parameters are also set here.
+  //   (overload for specifying proportional and derivative modes)
 
-  PID(double *, double *,
-      double *, // * constructor.  links the PID to the Input, Output, and
-      double, double, double,
-      int); //   Setpoint.  Initial tuning parameters are also set here
+  PID(double *Input, double *Output, double *Setpoint, double Kp, double Ki,
+      double Kd, int ControllerDirection);
+  // * constructor.  links the PID to the Input, Output, and
+  //   Setpoint.  Initial tuning parameters are also set here.
+  //   Uses default values DOn = D_ON_M and POn = P_ON_E
 
   void SetMode(int Mode); // * sets PID to either Manual (0) or Auto (non-0)
 
@@ -55,40 +56,38 @@ public:
                   //   calculation frequency can be set using SetMode
                   //   SetSampleTime respectively
 
-  void SetOutputLimits(
-      double,
-      double); // * clamps the output to a specific range. 0-255 by default, but
-               //   it's likely the user will want to change this depending on
-               //   the application
+  void SetOutputLimits(double Min, double Max);
+  // * clamps the output to a specific range. 0-255 by default, but
+  //   it's likely the user will want to change this depending on
+  //   the application
 
   // available but not commonly used functions
   // ********************************************************
-  void SetTunings(
-      double, double, // * While most users will set the tunings once in the
-      double);        //   constructor, this function gives the user the option
-               //   of changing tunings during runtime for Adaptive control
-  void SetTunings(double, double, // * overload for specifying proportional mode
-                  double, int);
+  void SetTunings(double Kp, double Ki, double Kd);
+  // * While most users will set the tunings once in the
+  //   constructor, this function gives the user the option
+  //   of changing tunings during runtime for Adaptive control
+  void SetTunings(double Kp, double Ki, double Kd, bool POn);
+  // * overload for specifying proportional mode
 
-  void SetTunings(double, double, // * overload for specifying derivative mode
-                  double, int, int);
+  void SetTunings(double Kp, double Ki, double Kd, bool POn, bool DOn);
+  // * overload for specifying proportional and derivative modes
 
-  void SetControllerDirection(
-      int); // * Sets the Direction, or "Action" of the controller. DIRECT
-            //   means the output will increase when error is positive. REVERSE
-            //   means the opposite.  it's very unlikely that this will be
-            //   needed once it is set in the constructor.
-
-  // Sets the frequency with which the PID calculation is performed.  Default
-  // is 100ms.
-  void SetSampleTime(Duration);
+  void SetControllerDirection(int Direction);
+  // * Sets the Direction, or "Action" of the controller. DIRECT
+  //   means the output will increase when error is positive. REVERSE
+  //   means the opposite.  it's very unlikely that this will be
+  //   needed once it is set in the constructor.
+  void SetSampleTime(Duration NewSampleTime);
+  // * sets the frequency, in Milliseconds, with which
+  //   the PID calculation is performed.  default is 100
 
   // Display functions
   // ****************************************************************
   double GetKp();     // These functions query the pid for interal values.
   double GetKi();     //  they were created mainly for the pid front-end,
   double GetKd();     // where it's important to know what is actually
-  int GetSampleTime();
+  Duration GetSampleTime();
   int GetMode();      //  inside the PID.
   int GetDirection(); //
 
@@ -100,18 +99,16 @@ private:
   double kd; // * (D)erivative Tuning Parameter
 
   int controllerDirection;
-  int pOn, dOn;
 
   double *myInput;  // * Pointers to the Input, Output, and Setpoint variables
   double *myOutput; //   This creates a hard link between the variables and the
-  double
-      *mySetpoint; //   PID, freeing the user from having to constantly tell us
-                   //   what these values are.  with pointers we'll just know.
+  double *mySetpoint; // PID, freeing the user from having to constantly tell
+                      // us what these values are.
 
-  Duration lastTime, lastSampleTime;
+  Duration nextSampleTime, lastUpdateTime;
   double outputSum, lastInput, lastError;
 
   double outMin, outMax;
-  bool inAuto, pOnE, dOnE;
+  bool inAuto, pOn, dOn;
 };
 #endif

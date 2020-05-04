38c253aea64faee50a4ec3d85f2a9c418337a944
asmodai27 <asmodai27@wanadoo.fr>
2020-05-04 19:42:25 +0200

    update to new time management system code review from jkff

--- controller/lib/pid/pid.h
+++ controller/lib/pid/pid.h
@@ -37,50 +37,50 @@ public:
 
   // commonly used functions
   // **************************************************************************
-  PID(double *Input, double *Output, double *Setpoint, double Kp, double Ki,
-      double Kd, bool POn, bool DOn, int ControllerDirection);
   // * constructor.  links the PID to the Input, Output, and
   //   Setpoint.  Initial tuning parameters are also set here.
   //   (overload for specifying proportional and derivative modes)
-
   PID(double *Input, double *Output, double *Setpoint, double Kp, double Ki,
-      double Kd, int ControllerDirection);
+      double Kd, bool POnE, bool DOnE, int ControllerDirection);
+
   // * constructor.  links the PID to the Input, Output, and
   //   Setpoint.  Initial tuning parameters are also set here.
-  //   Uses default values DOn = D_ON_M and POn = P_ON_E
+  //   Uses default values DOnE = D_ON_M and POnE = P_ON_E
+  PID(double *Input, double *Output, double *Setpoint, double Kp, double Ki,
+      double Kd, int ControllerDirection);
 
-  void SetMode(int Mode); // * sets PID to either Manual (0) or Auto (non-0)
+  // * sets PID to either Manual (0) or Auto (non-0)
+  void SetMode(int Mode);
 
-  bool Compute(); // * performs the PID calculation.  it should be
-                  //   called every time loop() cycles. ON/OFF and
-                  //   calculation frequency can be set using SetMode
-                  //   SetSampleTime respectively
+  // * performs the PID calculation.  it should be called every time loop()
+  // cycles. ON/OFF and calculation frequency can be set using SetMode
+  // SetSampleTime respectively
+  bool Compute();
 
-  void SetOutputLimits(double Min, double Max);
   // * clamps the output to a specific range. 0-255 by default, but
   //   it's likely the user will want to change this depending on
   //   the application
+  void SetOutputLimits(double Min, double Max);
 
   // available but not commonly used functions
   // ********************************************************
-  void SetTunings(double Kp, double Ki, double Kd);
   // * While most users will set the tunings once in the
   //   constructor, this function gives the user the option
   //   of changing tunings during runtime for Adaptive control
-  void SetTunings(double Kp, double Ki, double Kd, bool POn);
+  void SetTunings(double Kp, double Ki, double Kd);
   // * overload for specifying proportional mode
-
-  void SetTunings(double Kp, double Ki, double Kd, bool POn, bool DOn);
+  void SetTunings(double Kp, double Ki, double Kd, bool POnE);
   // * overload for specifying proportional and derivative modes
+  void SetTunings(double Kp, double Ki, double Kd, bool POnE, bool DOnE);
 
-  void SetControllerDirection(int Direction);
   // * Sets the Direction, or "Action" of the controller. DIRECT
   //   means the output will increase when error is positive. REVERSE
   //   means the opposite.  it's very unlikely that this will be
   //   needed once it is set in the constructor.
-  void SetSampleTime(Duration NewSampleTime);
+  void SetControllerDirection(int Direction);
   // * sets the frequency, in Milliseconds, with which
   //   the PID calculation is performed.  default is 100
+  void SetSampleTime(Duration NewSampleTime);
 
   // Display functions
   // ****************************************************************
@@ -88,8 +88,8 @@ public:
   double GetKi();     //  they were created mainly for the pid front-end,
   double GetKd();     // where it's important to know what is actually
   Duration GetSampleTime();
-  int GetMode();      //  inside the PID.
-  int GetDirection(); //
+  bool GetMode();      //  inside the PID.
+  bool GetDirection(); //
 
 private:
   void Initialize();
@@ -105,10 +105,11 @@ private:
   double *mySetpoint; // PID, freeing the user from having to constantly tell
                       // us what these values are.
 
-  Duration nextSampleTime, lastUpdateTime;
+  Duration SampleTime;
+  Time nextSampleTime, lastUpdateTime;
   double outputSum, lastInput, lastError;
 
   double outMin, outMax;
-  bool inAuto, pOn, dOn;
+  bool inAuto, pOnE, dOnE;
 };
 #endif

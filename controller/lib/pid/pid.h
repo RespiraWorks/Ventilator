a258acd73f3f05075a6affb0456a1c8a1fabe7cb
Eugene Kirpichov <ekirpichov@gmail.com>
2020-05-05 23:18:13 -0700

    Removes a bunch of useless code from PID

--- controller/lib/pid/pid.h
+++ controller/lib/pid/pid.h
@@ -22,12 +22,24 @@ limitations under the License.
 
 #include "units.h"
 
-class PID {
+enum class ProportionalTerm {
+  PROPORTIONAL_ON_ERROR,
+  PROPORTIONAL_ON_MEASUREMENT,
+};
 
+enum class DifferentialTerm {
+  DIFFERENTIAL_ON_ERROR,
+  DIFFERENTIAL_ON_MEASUREMENT,
+};
+
+enum class ControlDirection {
+  DIRECT,
+  REVERSE,
+};
+
+class PID {
 public:
 // Constants used in some of the functions below
-#define AUTOMATIC 1
-#define MANUAL 0
 #define DIRECT 0
 #define REVERSE 1
 #define P_ON_M 0
@@ -35,65 +47,28 @@ public:
 #define D_ON_M 0
 #define D_ON_E 1
 
-  // commonly used functions
-  // **************************************************************************
-  // * constructor.  links the PID to the Input, Output, and
-  //   Setpoint.  Initial tuning parameters are also set here.
-  //   (overload for specifying proportional and derivative modes)
+  // Constructs the PID linked to the Input, Output, and Setpoint,
+  // using the given parameters.
   PID(float *Input, float *Output, float *Setpoint, float Kp, float Ki,
       float Kd, bool POnE, bool DOnE, int ControllerDirection);
 
-  // * constructor.  links the PID to the Input, Output, and
-  //   Setpoint.  Initial tuning parameters are also set here.
-  //   Uses default values DOnE = D_ON_M and POnE = P_ON_E
-  PID(float *Input, float *Output, float *Setpoint, float Kp, float Ki,
-      float Kd, int ControllerDirection);
-
-  // * sets PID to either Manual (0) or Auto (non-0)
-  void SetMode(int Mode);
-
-  // * performs the PID calculation.  it should be called every time loop()
-  // cycles. ON/OFF and calculation frequency can be set using SetMode
-  // SetSampleTime respectively
+  // Performs one step of the PID calculation. Calculation frequency
+  // can be set using SetSampleTime.
   bool Compute();
 
-  // * clamps the output to a specific range. 0-255 by default, but
-  //   it's likely the user will want to change this depending on
-  //   the application
+  // Clamps the output to a specific range. 0-255 by default, but
+  // it's likely the user will want to change this depending on
+  // the application.
   void SetOutputLimits(float Min, float Max);
 
-  // available but not commonly used functions
-  // ********************************************************
-  // * While most users will set the tunings once in the
-  //   constructor, this function gives the user the option
-  //   of changing tunings during runtime for Adaptive control
-  void SetTunings(float Kp, float Ki, float Kd);
-  // * overload for specifying proportional mode
-  void SetTunings(float Kp, float Ki, float Kd, bool POnE);
-  // * overload for specifying proportional and derivative modes
+  // Changes the PID parameters to the given values.
   void SetTunings(float Kp, float Ki, float Kd, bool POnE, bool DOnE);
 
-  // * Sets the Direction, or "Action" of the controller. DIRECT
-  //   means the output will increase when error is positive. REVERSE
-  //   means the opposite.  it's very unlikely that this will be
-  //   needed once it is set in the constructor.
-  void SetControllerDirection(int Direction);
-  // * sets the frequency, in Milliseconds, with which
-  //   the PID calculation is performed.  default is 100
+  // Sets the frequency, in Milliseconds, with which the PID calculation
+  // is performed.  Default is 100.
   void SetSampleTime(Duration NewSampleTime);
 
-  // Display functions
-  // ****************************************************************
-  float GetKp(); // These functions query the pid for interal values.
-  float GetKi(); //  they were created mainly for the pid front-end,
-  float GetKd(); // where it's important to know what is actually
-  Duration GetSampleTime();
-  bool GetMode();      //  inside the PID.
-  bool GetDirection(); //
-
 private:
-  void Initialize();
-
   float kp; // * (P)roportional Tuning Parameter
   float ki; // * (I)ntegral Tuning Parameter
   float kd; // * (D)erivative Tuning Parameter
@@ -110,6 +85,6 @@ private:
   float outputSum, lastInput, lastError;
 
   float outMin, outMax;
-  bool inAuto, pOnE, dOnE;
+  bool pOnE, dOnE;
 };
 #endif

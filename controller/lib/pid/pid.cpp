55c215f7e8e46bbecc5247445d923af051732d2c
Eugene Kirpichov <ekirpichov@gmail.com>
2020-05-05 23:18:13 -0700

    Removes a bunch of useless code from PID

--- controller/lib/pid/pid.cpp
+++ controller/lib/pid/pid.cpp
@@ -20,10 +20,6 @@ limitations under the License.
 #include "pid.h"
 #include "hal.h"
 
-/*Constructor (...)*********************************************************
- *    The parameters specified here are those for for which we can't set up
- *    reliable defaults, so we need to have the user set them.
- ***************************************************************************/
 PID::PID(float *Input, float *Output, float *Setpoint, float Kp, float Ki,
          float Kd, bool POnE, bool DOnE, int ControllerDirection)
     : SampleTime(milliseconds(100)), nextSampleTime(Hal.now()),
@@ -31,24 +27,13 @@ PID::PID(float *Input, float *Output, float *Setpoint, float Kp, float Ki,
   myOutput = Output;
   myInput = Input;
   mySetpoint = Setpoint;
-  inAuto = false;
 
   // default output limit corresponds to the arduino pwm limits
   PID::SetOutputLimits(0, 255);
 
-  PID::SetControllerDirection(ControllerDirection);
+  controllerDirection = ControllerDirection;
   PID::SetTunings(Kp, Ki, Kd, POnE, DOnE);
 }
-
-/*Constructor (...)*********************************************************
- *    To allow backwards compatability for v1.1, or for people that just want
- *    to use Proportional on Error without explicitly saying so
- ***************************************************************************/
-
-PID::PID(float *Input, float *Output, float *Setpoint, float Kp, float Ki,
-         float Kd, int ControllerDirection)
-    : PID::PID(Input, Output, Setpoint, Kp, Ki, Kd, P_ON_E, D_ON_M,
-               ControllerDirection) {}
 
 /* Compute()
  *********************************************************************** This,
@@ -58,66 +43,60 @@ PID::PID(float *Input, float *Output, float *Setpoint, float Kp, float Ki,
  *computed, false when nothing has been done.
  **********************************************************************************/
 bool PID::Compute() {
-  if (!inAuto)
-    return false;
   Time now = Hal.now();
   // compute actual samples time-difference to take jitter into account in
   // integral and derivative
   Duration effectiveSampleTime = (now - lastUpdateTime);
   float samplesTimeChangeSec = effectiveSampleTime.seconds();
   // condition to update output : 1 sample time has passed and we have new data
-  if (now >= nextSampleTime && samplesTimeChangeSec > 0) {
-    /*Compute all the working error variables*/
-    float input = *myInput;
-    float error = *mySetpoint - input;
-    float dInput = 0.0;
-    // Compute dInput only if needed (P_ON_M or D_ON_M)
-    if (pOnE == P_ON_M || dOnE == D_ON_M) {
-      dInput = (input - lastInput);
-    }
-    outputSum += (ki * error * samplesTimeChangeSec);
-
-    /*Add Proportional on Measurement, if P_ON_M is specified*/
-    if (pOnE == P_ON_M)
-      outputSum -= kp * dInput;
-
-    if (outputSum > outMax)
-      outputSum = outMax;
-    else if (outputSum < outMin)
-      outputSum = outMin;
-
-    /*Add Proportional on Error, if P_ON_E is specified*/
-    float output;
-    if (pOnE == P_ON_E) {
-      output = kp * error;
-    } else {
-      output = 0;
-    }
-    if (dOnE == D_ON_M) {
-      dInput /= samplesTimeChangeSec;
-      /*Compute Rest of PID Output*/
-      output += outputSum - kd * dInput;
-    } else { // dOnE==D_ON_E
-      float dError = (error - lastError) / samplesTimeChangeSec;
-      /*Compute Rest of PID Output*/
-      output += outputSum + kd * dError;
-    }
-
-    if (output > outMax)
-      output = outMax;
-    else if (output < outMin)
-      output = outMin;
-    *myOutput = output;
-
-    /*Remember some variables for next time*/
-    lastInput = input;
-    lastError = error;
-    lastUpdateTime = now;
-    // when should we expect to perform our next output calculation
-    nextSampleTime = nextSampleTime + SampleTime;
-    return true;
-  } else
+  if (now < nextSampleTime || samplesTimeChangeSec <= 0) {
     return false;
+  }
+  /*Compute all the working error variables*/
+  float input = *myInput;
+  float error = *mySetpoint - input;
+  float dInput = 0.0;
+  // Compute dInput only if needed (P_ON_M or D_ON_M)
+  if (pOnE == P_ON_M || dOnE == D_ON_M) {
+    dInput = (input - lastInput);
+  }
+  outputSum += (ki * error * samplesTimeChangeSec);
+
+  /*Add Proportional on Measurement, if P_ON_M is specified*/
+  if (pOnE == P_ON_M)
+    outputSum -= kp * dInput;
+
+  if (outputSum > outMax)
+    outputSum = outMax;
+  else if (outputSum < outMin)
+    outputSum = outMin;
+
+  /*Add Proportional on Error, if P_ON_E is specified*/
+  float output;
+  if (pOnE == P_ON_E) {
+    output = kp * error;
+  } else {
+    output = 0;
+  }
+  if (dOnE == D_ON_M) {
+    dInput /= samplesTimeChangeSec;
+    /*Compute Rest of PID Output*/
+    output += outputSum - kd * dInput;
+  } else { // dOnE==D_ON_E
+    float dError = (error - lastError) / samplesTimeChangeSec;
+    /*Compute Rest of PID Output*/
+    output += outputSum + kd * dError;
+  }
+
+  *myOutput = std::clamp(output, outMin, outMax);
+
+  /*Remember some variables for next time*/
+  lastInput = input;
+  lastError = error;
+  lastUpdateTime = now;
+  // when should we expect to perform our next output calculation
+  nextSampleTime = nextSampleTime + SampleTime;
+  return true;
 }
 
 /* SetTunings(...)*************************************************************
@@ -130,7 +109,6 @@ void PID::SetTunings(float Kp, float Ki, float Kd, bool POnE, bool DOnE) {
     return;
 
   pOnE = POnE;
-
   dOnE = DOnE;
 
   kp = Kp;
@@ -144,28 +122,15 @@ void PID::SetTunings(float Kp, float Ki, float Kd, bool POnE, bool DOnE) {
   }
 }
 
-/* SetTunings(...)*************************************************************
- * Set Tunings using the last-rembered  DOnE setting
- ******************************************************************************/
-void PID::SetTunings(float Kp, float Ki, float Kd, bool POnE) {
-  SetTunings(Kp, Ki, Kd, POnE, dOnE);
-}
-
-/* SetTunings(...)*************************************************************
- * Set Tunings using the last-rembered POnE and DOnE setting
- ******************************************************************************/
-void PID::SetTunings(float Kp, float Ki, float Kd) {
-  SetTunings(Kp, Ki, Kd, pOnE, dOnE);
-}
-
 /* SetSampleTime(...) *********************************************************
  * sets the period, in Milliseconds, at which the calculation is performed
  ******************************************************************************/
 void PID::SetSampleTime(Duration NewSampleTime) {
-  if (NewSampleTime > milliseconds(0)) {
-    nextSampleTime = nextSampleTime - SampleTime + NewSampleTime;
-    SampleTime = NewSampleTime;
+  if (NewSampleTime <= milliseconds(0)) {
+    return;
   }
+  nextSampleTime = nextSampleTime - SampleTime + NewSampleTime;
+  SampleTime = NewSampleTime;
 }
 
 /* SetOutputLimits(...)****************************************************
@@ -182,74 +147,6 @@ void PID::SetOutputLimits(float Min, float Max) {
   outMin = Min;
   outMax = Max;
 
-  if (inAuto) {
-    if (*myOutput > outMax)
-      *myOutput = outMax;
-    else if (*myOutput < outMin)
-      *myOutput = outMin;
-
-    if (outputSum > outMax)
-      outputSum = outMax;
-    else if (outputSum < outMin)
-      outputSum = outMin;
-  }
+  *myOutput = std::clamp(*myOutput, outMin, outMax);
+  outputSum = std::clamp(outputSum, outMin, outMax);
 }
-
-/* SetMode(...)****************************************************************
- * Allows the controller Mode to be set to manual (0) or Automatic (non-zero)
- * when the transition from manual to auto occurs, the controller is
- * automatically initialized
- ******************************************************************************/
-void PID::SetMode(int Mode) {
-  bool newAuto = (Mode == AUTOMATIC);
-  if (newAuto && !inAuto) { /*we just went from manual to auto*/
-    PID::Initialize();
-  }
-  inAuto = newAuto;
-}
-
-/* Initialize()****************************************************************
- *	does all the things that need to happen to ensure a bumpless transfer
- *  from manual to automatic mode.
- ******************************************************************************/
-void PID::Initialize() {
-  outputSum = *myOutput;
-  lastInput = *myInput;
-  lastError = *mySetpoint - *myInput;
-  if (outputSum > outMax)
-    outputSum = outMax;
-  else if (outputSum < outMin)
-    outputSum = outMin;
-  // next sample time in now
-  nextSampleTime = Hal.now();
-  // last call time defined as now - SampleTime to enable computation on first
-  // call (user should call Compute() immediately after SetMode(Auto))
-  lastUpdateTime = Hal.now() - SampleTime;
-}
-
-/* SetControllerDirection(...)*************************************************
- * The PID will either be connected to a DIRECT acting process (+Output leads
- * to +Input) or a REVERSE acting process(+Output leads to -Input.)  we need to
- * know which one, because otherwise we may increase the output when we should
- * be decreasing.  This is called from the constructor.
- ******************************************************************************/
-void PID::SetControllerDirection(int Direction) {
-  if (inAuto && Direction != controllerDirection) {
-    kp = (0 - kp);
-    ki = (0 - ki);
-    kd = (0 - kd);
-  }
-  controllerDirection = Direction;
-}
-
-/* Status Funcions*************************************************************
- * Just because you set the Kp=-1 doesn't mean it actually happened.  these
- * functions query the internal state of the PID.  they're here for display
- * purposes.  this are the functions the PID Front-end uses for example
- ******************************************************************************/
-float PID::GetKp() { return kp; }
-float PID::GetKi() { return ki; }
-float PID::GetKd() { return kd; }
-Duration PID::GetSampleTime() { return SampleTime; }
-bool PID::GetMode() { return inAuto ? AUTOMATIC : MANUAL; }
-bool PID::GetDirection() { return controllerDirection; }

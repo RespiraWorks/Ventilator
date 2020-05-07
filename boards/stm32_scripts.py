addaab0c652389227e97b2c39c4077d35b6676be
Steve Glow <sglow@embeddedintelligence.com>
2020-05-07 10:48:58 -0400

    Added readme file to describe what's in this directory. Added a comment to stm32_scripts.py

--- boards/stm32_scripts.py
+++ boards/stm32_scripts.py
@@ -1,4 +1,13 @@
-
+# This script is called as part of the platformio build sequence for
+# the stm32 controller firmware.
+#
+# It's purpose is to add some additional linker options which can't be
+# directly added to the platformio.ini line for some reason.
+#
+# These link flags tell the linker that the processor we're using has 
+# hardware floating point, so the software floating point libraries aren't 
+# needed.
+# 
 Import( "env" )
 
 env.Append(

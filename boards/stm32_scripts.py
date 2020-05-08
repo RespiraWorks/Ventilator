95a372a66cc5502bdeeefb1e82f08af9d6a05ad5
Steve Glow <sglow@embeddedintelligence.com>
2020-05-07 21:17:34 -0400

    Removed some spaces at end of lines

--- boards/stm32_scripts.py
+++ boards/stm32_scripts.py
@@ -4,10 +4,10 @@
 # It's purpose is to add some additional linker options which can't be
 # directly added to the platformio.ini line for some reason.
 #
-# These link flags tell the linker that the processor we're using has 
-# hardware floating point, so the software floating point libraries aren't 
+# These link flags tell the linker that the processor we're using has
+# hardware floating point, so the software floating point libraries aren't
 # needed.
-# 
+#
 Import( "env" )
 
 env.Append(

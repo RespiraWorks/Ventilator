32eabe0cdf66060235f3cdfd29c2119afb14e438
Steve Glow <sglow@embeddedintelligence.com>
2020-05-07 21:43:12 -0400

    More clang foolishness

--- controller/lib/hal/hal_stm32.h
+++ controller/lib/hal/hal_stm32.h
@@ -80,27 +80,28 @@ typedef volatile uint8_t BREG;
 
 // Reset & clock controller
 #define RCC_BASE                    0x40021000
-struct RCC_Regs
-{
-   REG clkCtrl;                  // 0x00 clock control register (RCC_CR)
-   REG clkCal;                   // 0x04 Internal clock sources calibration register (RCC_ICSCR)
-   REG clkCfg;                   // 0x08 clock configuration register (RCC_CFGR)
-   REG pllCfg;                   // 0x0C PLL configuration register (RCC_PLLCFGR)
-   REG pllSaiCfg;                // 0x10 PLLSAI1 configuration register (RCC_PLLSAI1CFGR)
-   REG rsvd1;
-   REG clkIntEna; // 0x18 Clock interrupt enable register ( RCC_CIER)
-   REG clkIntFlg;                // 0x1C Clock interrupt flag register ( RCC_CIFR)
-   REG clkIntClr;                // 0x20 Clock interrupt clear register ( RCC_CICR)
-   REG rsvd2;
-   REG periphReset[8];           // 0x28 peripheral reset registers
-   REG periphClkEna[8];          // 0x48 peripheral clock registers
-   REG sleepClkEna[8];           // 0x68 Clock enable in sleep
-   REG indClkCfg;                // 0x88 Peripherals independent clock configuration register (RCC_CCIPR)
-   REG rsvd3;
-   REG backup;                   // 0x90 Backup domain control register (RCC_BDCR)
-   REG status;                   // 0x94 control & status register (RCC_CSR)
-   REG recovery;                 // 0x98 Clock recovery RC register (RCC_CRRCR)
-   REG indClkCfg2;               // 0x9C Peripherals independent clock configuration register (RCC_CCIPR2)
+struct RCC_Regs {
+  REG clkCtrl;   // 0x00 clock control register (RCC_CR)
+  REG clkCal;    // 0x04 Internal clock sources calibration register (RCC_ICSCR)
+  REG clkCfg;    // 0x08 clock configuration register (RCC_CFGR)
+  REG pllCfg;    // 0x0C PLL configuration register (RCC_PLLCFGR)
+  REG pllSaiCfg; // 0x10 PLLSAI1 configuration register (RCC_PLLSAI1CFGR)
+  REG rsvd1;
+  REG clkIntEna; // 0x18 Clock interrupt enable register ( RCC_CIER)
+  REG clkIntFlg; // 0x1C Clock interrupt flag register ( RCC_CIFR)
+  REG clkIntClr; // 0x20 Clock interrupt clear register ( RCC_CICR)
+  REG rsvd2;
+  REG periphReset[8];  // 0x28 peripheral reset registers
+  REG periphClkEna[8]; // 0x48 peripheral clock registers
+  REG sleepClkEna[8];  // 0x68 Clock enable in sleep
+  REG indClkCfg; // 0x88 Peripherals independent clock configuration register
+                 // (RCC_CCIPR)
+  REG rsvd3;
+  REG backup;     // 0x90 Backup domain control register (RCC_BDCR)
+  REG status;     // 0x94 control & status register (RCC_CSR)
+  REG recovery;   // 0x98 Clock recovery RC register (RCC_CRRCR)
+  REG indClkCfg2; // 0x9C Peripherals independent clock configuration register
+                  // (RCC_CCIPR2)
 };
 
 // System control registers

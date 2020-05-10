d7ebecf339e9af6192cbac12db56985f1dfde066
Hugh Francis <hugh@sanctuary.computer>
2020-05-10 14:32:33 -0400

    Remove travis from repo, closes #270

--- test.sh
+++ test.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Use this script during development to have a good approximation
-# of whether your PR will pass tests on Travis:
+# of whether your PR will pass tests on CircleCI:
 #
 # $ ./test.sh
 #
-# Travis runs this script (via .travis.yml), but might have some
-# environment differences, so the approximation is not perfect.
+# CircleCI runs this script (via .travis.yml), but might have some
+# environment differences, so the approximation is not perfect. For
+# a more exact test run, please build the tests_Dockerfile.
 #
 # Feel free to add more checks here, but keep in mind that:
-# - They have to pass on Travis
+# - They have to pass on CircleCI
 # - They have to have a very good chance of passing for other
 #   developers if they run via ./test.sh
 

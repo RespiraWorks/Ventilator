607d221b568f15cbcc058f1a63bcc39b3a8bd649
asmodai27 <asmodai27@wanadoo.fr>
2020-05-10 12:19:59 +0200

    remove unnecessary includes from blower_fsm and blower_pid

--- controller/lib/core/actuators.cpp
+++ controller/lib/core/actuators.cpp
@@ -26,5 +26,5 @@ void actuators_execute(const ActuatorsState &desired_state) {
   // set blower PWM
   // TODO: create PWM limits in HAL (255) and use this instead
   Hal.analogWrite(PwmPin::BLOWER,
-                  static_cast<int>(desired_state.fan_power * 255));
+                  static_cast<int>(desired_state.fan_power * 255.0f));
 }

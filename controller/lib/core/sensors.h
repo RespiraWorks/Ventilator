96d615c850a7573ec78f43e6a7024d06765aa0e5
Eugene Kirpichov <ekirpichov@gmail.com>
2020-05-10 19:51:41 -0700

    Makes Sensors a proper class with no static state, only a static instance in main.cpp

--- controller/lib/core/sensors.h
+++ controller/lib/core/sensors.h
@@ -26,44 +26,74 @@ Arduino Nano and the MPXV5004GP and MPXV7002DP pressure sensors.
 #include "network_protocol.pb.h"
 #include "units.h"
 
-// A namespace class for constants related to pressure sensors.
-class PressureSensors {
+class TVIntegrator {
 public:
-  PressureSensors() = delete;
+  void AddFlow(Time now, VolumetricFlow flow);
+  Volume GetTV() const { return volume_; }
+
+private:
+  Time last_flow_measurement_time_ = millisSinceStartup(0);
+  VolumetricFlow last_flow_ = cubic_m_per_sec(0);
+  Volume volume_ = ml(0);
+  bool initialized_ = false;
+};
+
+// Provides calibrated sensor readings, including tidal volume (TV)
+// integrated from flow.
+class Sensors {
+public:
+  Sensors() {}
+
+  // Calibrates the sensors. Must call this before calling any other methods.
+  void Init();
+
+  // Gets gets the current pressure at the patient.
+  Pressure GetPatientPressure();
+  // Gets the volumetric inflow (i.e. rate of air flowing into the patient).
+  VolumetricFlow GetVolumetricInflow();
+  // Gets the volumetric outflow (i.e. rate of air flowing out of the patient).
+  VolumetricFlow GetVolumetricOutflow();
+
+  // get the sensor readings (patient pressure, volumetric flow and tidal
+  // volume) from the sensors
+  SensorReadings GetSensorReadings();
 
   // min/max possible reading from MPXV5004GP pressure sensors
   // The canonical list of hardware in the device is: https://bit.ly/3aERr69
   inline constexpr static Pressure P_VAL_MIN = kPa(0.0f);
   inline constexpr static Pressure P_VAL_MAX = kPa(3.92f);
+
+  /*
+   * @brief Method implements Bernoulli's equation assuming the Venturi Effect.
+   * https://en.wikipedia.org/wiki/Venturi_effect
+   *
+   * Solves for the volumetric flow rate since A1/A2, rho, and differential
+   * pressure are known. Q = sqrt(2/rho) * (A1*A2) * 1/sqrt(A1^2-A2^2) *
+   * sqrt(p1-p2); based on (p1 - p2) = (rho/2) * (v2^2 - v1^2); where A1 > A2
+   *
+   * @return the volumetric flow in [meters^3/s]. Can be negative, indicating
+   * direction of flow, depending on how the differential sensor is attached to
+   * the venturi.
+   */
+  static VolumetricFlow PressureDeltaToFlow(Pressure delta);
+
+private:
+  enum Sensor {
+    PATIENT_PRESSURE,
+    INFLOW_PRESSURE_DIFF,
+    OUTFLOW_PRESSURE_DIFF,
+  };
+  // Keep this in sync with the Sensor enum!
+  inline constexpr static int NUM_SENSORS = 3;
+
+  static AnalogPin PinFor(Sensor s);
+  Pressure ReadPressureSensor(Sensor s);
+
+  // Calibrated average sensor values in a zero state.
+  float sensors_zero_vals_[NUM_SENSORS];
+
+  // State related to integrating volume from flow.
+  TVIntegrator tv_integrator_;
 };
-
-void sensors_init();
-
-// Gets gets the current pressure at the patient, in kPa.
-Pressure get_patient_pressure();
-
-// Gets the volumetric inflow (i.e. rate of air flowing into the patient).
-VolumetricFlow get_volumetric_inflow();
-
-// Gets the volumetric outflow (i.e. rate of air flowing out of the patient).
-VolumetricFlow get_volumetric_outflow();
-
-/*
- * @brief Method implements Bernoulli's equation assuming the Venturi Effect.
- * https://en.wikipedia.org/wiki/Venturi_effect
- *
- * Solves for the volumetric flow rate since A1/A2, rho, and differential
- * pressure are known. Q = sqrt(2/rho) * (A1*A2) * 1/sqrt(A1^2-A2^2) *
- * sqrt(p1-p2); based on (p1 - p2) = (rho/2) * (v2^2 - v1^2); where A1 > A2
- *
- * @return the volumetric flow in [meters^3/s]. Can be negative, indicating
- * direction of flow, depending on how the differential sensor is attached to
- * the venturi.
- */
-VolumetricFlow pressure_delta_to_flow(Pressure delta);
-
-// get the sensor readings (patient pressure, volumetric flow and tidal
-// volume) from the sensors
-SensorReadings get_sensor_readings();
 
 #endif // SENSORS_H

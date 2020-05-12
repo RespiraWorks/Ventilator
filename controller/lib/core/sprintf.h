c19d4f707d53ad496fd3bc29588b93585f18953e
Steve Glow <sglow@embeddedintelligence.com>
2020-05-12 11:05:11 -0400

    Removed some incorrect info from license comment

--- controller/lib/core/sprintf.h
+++ controller/lib/core/sprintf.h
@@ -11,15 +11,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
-This file implements the HAL (Hardware Abstraction Layer) for the
-STM32L452 processor used on the controller.  Details of the processor's
-peripherals can be found in the reference manual for that processor:
-   https://www.st.com/resource/en/reference_manual/dm00151940-stm32l41xxx42xxx43xxx44xxx45xxx46xxx-advanced-armbased-32bit-mcus-stmicroelectronics.pdf
-
-Details specific to the ARM processor used in this chip can be found in
-the programmer's manual for the processor available here:
-   https://www.st.com/resource/en/programming_manual/dm00046982-stm32-cortexm4-mcus-and-mpus-programming-manual-stmicroelectronics.pdf
 */
 
 #ifndef SPRINTF_H

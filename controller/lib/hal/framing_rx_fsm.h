866998627472389ea789d893aa850435e8632928
miceuz <mic@hardcore.lt>
2020-05-18 13:37:30 +0300

    Rename

--- controller/lib/hal/framing_rx_fsm.h
+++ controller/lib/hal/framing_rx_fsm.h
@@ -5,22 +5,22 @@ extern UART_DMA dmaUART;
 
 class FramingRxFSM : public UART_DMA_RxListener {
   enum State_t { STATE_LOST, STATE_WAIT_START, STATE_RX_FRAME };
-  static constexpr uint32_t RX_BYTES_MAX = 10;
   static constexpr uint32_t RX_TIMEOUT = 115200 * 10;
-  static constexpr uint32_t TX_BUF_LEN = (GuiStatus_size + 4) * 2 + 2;
 
   State_t state = STATE_LOST;
   uint32_t errorCounter = 0;
   // Size of the buffer is set asuming a corner case where EVERY GuiStatus
   // byte and CRC32 will be escaped + two marker chars; this is too big, but
   // safe.
-  uint8_t rx_buf[];
+  static constexpr uint32_t RX_BUF_LEN = (GuiStatus_size + 4) * 2 + 2;
+  static constexpr uint32_t RX_BYTES_MAX = RX_BUF_LEN;
+  uint8_t rx_buf[RX_BUF_LEN];
 
 public:
   void begin() {
     state = STATE_LOST;
     dmaUART.charMatchEnable();
-    dmaUART.startRX(r, RX_BYTES_MAX, RX_TIMEOUT);
+    dmaUART.startRX(rx_buf, RX_BYTES_MAX, RX_TIMEOUT);
   }
 
   void onRxComplete() {

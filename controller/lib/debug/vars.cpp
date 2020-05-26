14335ddc19dfc6d0af2828799838a43a9fc424f5
Eugene Kirpichov <ekirpichov@gmail.com>
2020-05-25 19:08:08 -0700

    Changes debug vars to be adapters over uint32

--- controller/lib/debug/vars.cpp
+++ controller/lib/debug/vars.cpp
@@ -15,65 +15,9 @@ limitations under the License.
 
 #include "vars.h"
 #include "debug.h"
+#include <cstring>
 #include <string.h>
 
 // static member variables
 DebugVar *DebugVar::varList[100];
 int DebugVar::varCount = 0;
-
-// Used to read a debug variable's value over the debug serial interface
-DbgErrCode DebugVar::GetValue(uint8_t *buff, int *len, int max) {
-  switch (type_) {
-  // We can actually treat 32-bit ints and floats exactly the same way.
-  // We're just grabbing their value directly from memory and treating it
-  // as an integer so we can split it up and send it out.
-  case VarType::INT32:
-  case VarType::UINT32:
-  case VarType::FLOAT:
-    *len = 4;
-    if (max < 4)
-      return DbgErrCode::NO_MEMORY;
-
-    u32_to_u8(*reinterpret_cast<uint32_t *>(addr_), buff);
-    return DbgErrCode::OK;
-
-  // For other types we just generate an error.
-  // Anything exotic needs to override this function
-  default:
-    return DbgErrCode::INTERNAL;
-  }
-}
-
-// Used to set a debug variable's value over the debug serial interface
-DbgErrCode DebugVar::SetValue(uint8_t *buff, int len) {
-  switch (type_) {
-  // Like with the get, we actually handle both floats and ints the same here.
-  case VarType::INT32:
-  case VarType::UINT32:
-  case VarType::FLOAT:
-    if (len < 4)
-      return DbgErrCode::MISSING_DATA;
-
-    *reinterpret_cast<uint32_t *>(addr_) = u8_to_u32(buff);
-    return DbgErrCode::OK;
-
-  default:
-    return DbgErrCode::INTERNAL;
-  }
-}
-
-// Return the variable as a 32-bit integer.
-// We just return zero for any type we don't understand
-uint32_t DebugVar::getDataForTrace() {
-  switch (type_) {
-
-  // Like above, we treat 32-bit ints and floats the same here
-  case VarType::INT32:
-  case VarType::UINT32:
-  case VarType::FLOAT:
-    return *reinterpret_cast<uint32_t *>(addr_);
-
-  default:
-    return 0;
-  }
-}

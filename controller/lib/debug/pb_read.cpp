59d4d8d3caac6c5b7eb9b9993289a52155edf26e
Eugene Kirpichov <ekirpichov@gmail.com>
2020-05-25 18:08:45 -0700

    Makes ./test.sh pass with clang.
    This includes one bug (= instead of ==) and something that is most
    likely undefined behavior but somehow worked: all the debug cmds
    were declared as static variables of type "FooCmd" in their respective
    sources, but then as "extern DebugCmd" (different type) in debug.cpp.
    
    git-pr-chain: test-sh

--- controller/lib/debug/pb_read.cpp
+++ controller/lib/debug/pb_read.cpp
@@ -13,37 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-/*
- * This file implements a command that allows the contents of the
- * print buffer data to be read.  The print buffer buffer is
- * where strings go when you call debug.Print
- */
+#include "pb_read.h"
 
-#include "debug.h"
-#include <optional>
-#include <string.h>
-
-class PrintBuffReadCmd : public DebugCmd {
-public:
-  PrintBuffReadCmd() : DebugCmd(DbgCmdCode::PRINT_BUFF_READ) {}
-
-  DbgErrCode HandleCmd(uint8_t *data, int *len, int max) {
-    // No data needs to be passed in to this command.
-
-    // Read bytes from the print buffer until I hit my
-    // max or the buffer is empty.
-
-    int i;
-    for (i = 0; i < max; i++) {
-      std::optional<uint8_t> ch = debug.PrintBuffGet();
-      if (ch = std::nullopt)
-        break;
-      *data++ = *ch;
-    }
-
-    *len = i;
-    return DbgErrCode::OK;
-  }
-};
-
-PrintBuffReadCmd pbRead;
+PrintBuffReadCmd pbReadCmd;

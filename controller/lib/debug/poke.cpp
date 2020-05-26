5f82255f155215e392cf346409401c3d5bef790f
Eugene Kirpichov <ekirpichov@gmail.com>
2020-05-25 18:08:45 -0700

    Makes ./test.sh pass with clang.
    This includes one bug (= instead of ==) and something that is most
    likely undefined behavior but somehow worked: all the debug cmds
    were declared as static variables of type "FooCmd" in their respective
    sources, but then as "extern DebugCmd" (different type) in debug.cpp.
    
    git-pr-chain: test-sh

--- controller/lib/debug/poke.cpp
+++ controller/lib/debug/poke.cpp
@@ -23,50 +23,6 @@ limitations under the License.
  * given address
  */
 
-#include "debug.h"
-#include <string.h>
+#include "poke.h"
 
-class PokeCmd : public DebugCmd {
-public:
-  PokeCmd() : DebugCmd(DbgCmdCode::POKE) {}
-  DbgErrCode HandleCmd(uint8_t *data, int *len, int max) {
-
-    // Total command length must be at least 5.  That's
-    // four for the address and at least one data byte.
-    if (*len < 5)
-      return DbgErrCode::MISSING_DATA;
-
-    uint32_t addr = u8_to_u32(&data[0]);
-
-    int ct = *len - 4;
-
-    // If both the address and count are multiples of 4, I write
-    // 32-bit values.  This is useful when poking into registers
-    // that need to be written as longs.
-    if (!(addr & 3) && !(ct & 3)) {
-      uint32_t *ptr = reinterpret_cast<uint32_t *>(addr);
-      ct /= 4;
-      for (int i = 0; i < ct; i++)
-        *ptr++ = u8_to_u32(&data[4 + i * 4]);
-    }
-
-    // Same idea for multiples of 2
-    else if (!(addr & 1) && !(ct & 1)) {
-      uint16_t *ptr = reinterpret_cast<uint16_t *>(addr);
-      ct /= 2;
-      for (int i = 0; i < ct; i++)
-        *ptr++ = u8_to_u16(&data[4 + i * 2]);
-    }
-
-    else {
-      uint8_t *ptr = reinterpret_cast<uint8_t *>(addr);
-      for (int i = 0; i < ct; i++)
-        *ptr++ = data[4 + i];
-    }
-
-    *len = 0;
-    return DbgErrCode::OK;
-  }
-};
-
-PokeCmd poke;
+PokeCmd pokeCmd;

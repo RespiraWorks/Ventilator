77197b537d2ab253eb7fb03fdf75f7c219d03f05
Eugene Kirpichov <ekirpichov@gmail.com>
2020-05-27 22:31:04 -0700

    Puts tracing into a class.
    git-pr-chain: trace-class

--- controller/lib/debug/trace.cpp
+++ controller/lib/debug/trace.cpp
@@ -31,67 +31,55 @@ limitations under the License.
 #include <optional>
 #include <stdint.h>
 
-CircBuff<uint32_t, 0x4000> traceBuffer;
-int32_t traceCtrl;
-int32_t tracePeriod;
-int32_t traceCount;
-int32_t traceSamp;
-int32_t traceVar[TRACE_VAR_CT];
-
-class TraceCtrlVar : public DebugVar {
-public:
-  TraceCtrlVar()
-      : DebugVar("trace_ctrl", &traceCtrl,
-                 "Used to start/stop the trace function", "0x%08x") {}
-  void SetValue(uint32_t value) override;
-};
+CircBuff<uint32_t, 0x4000> trace_buffer;
+int32_t trace_var[TRACE_VAR_CT];
+TraceControl trace_control;
 
 // These variables are used control the trace function
-static TraceCtrlVar varTraceCtrl;
-static DebugVar
-    varTracePeriod("trace_period", &tracePeriod,
-                   "Period that data will be captured.  Loop cycle units");
-static DebugVar varTraceSamp("trace_samples", &traceSamp,
-                             "Number of trace samples saved so far");
-static DebugVar varTraceVar1("trace_var1", &traceVar[0],
-                             "Variable to be saved to the trace buffer");
-static DebugVar varTraceVar2("trace_var2", &traceVar[1],
-                             "Variable to be saved to the trace buffer");
-static DebugVar varTraceVar3("trace_var3", &traceVar[2],
-                             "Variable to be saved to the trace buffer");
-static DebugVar varTraceVar4("trace_var4", &traceVar[3],
-                             "Variable to be saved to the trace buffer");
+static FnDebugVar varTraceCtrl(
+    VarType::UINT32, "trace_ctrl", "Used to start/stop the trace function",
+    "0x%08x", [] { return trace_control.GetFlags(); },
+    [](uint32_t value) { trace_control.SetFlags(value); });
+
+static FnDebugVar varTracePeriod(
+    VarType::UINT32, "trace_period",
+    "Period that data will be captured.  Loop cycle units", "%u",
+    [] { return trace_control.GetPeriod(); },
+    [](uint32_t value) { trace_control.SetPeriod(value); });
+
+static FnDebugVar varTraceSamp(
+    VarType::UINT32, "trace_samples", "Number of trace samples saved so far",
+    "%u", [] { return trace_buffer.FullCt() / CountActiveVars(nullptr); },
+    [](uint32_t value) { /* ignore */ });
+
+static DebugVar vartrace_var1("trace_var1", &trace_var[0],
+                              "Variable to be saved to the trace buffer");
+static DebugVar vartrace_var2("trace_var2", &trace_var[1],
+                              "Variable to be saved to the trace buffer");
+static DebugVar vartrace_var3("trace_var3", &trace_var[2],
+                              "Variable to be saved to the trace buffer");
+static DebugVar vartrace_var4("trace_var4", &trace_var[3],
+                              "Variable to be saved to the trace buffer");
 
 // This function is called at the end of the high priority loop function.
 // It captures any enabled data variables to the trace buffer.
-void TraceSample() {
-
-  // Make sure the trace is running, if not we're done
-  if (!(traceCtrl & 1))
-    return;
-
-  // Count down the trace period
-  if (++traceCount < tracePeriod)
-    return;
-  traceCount = 0;
-
+bool TraceControl::SampleAllVars() {
   // Count the number of active trace variables with a
-  // These will have a non-negative traceVar value.
-  DebugVar *vptr[TRACE_VAR_CT];
+  // These will have a non-negative trace_var value.
+  DebugVarBase *vptr[TRACE_VAR_CT];
   int vct = CountActiveVars(vptr);
 
   // If there are no enabled trace variables, or
   // if there isn't enough space in the buffer for
   // a full sample, then stop the trace
-  if (!vct || (traceBuffer.FreeCt() < vct)) {
-    traceCtrl = 0;
-    return;
+  if (!vct || (trace_buffer.FreeCt() < vct)) {
+    return false;
   }
 
   // Sample each enabled varible and store the
   // result to the buffer
   for (int i = 0; i < vct; i++) {
-    bool ok = traceBuffer.Put(vptr[i]->GetValue());
+    bool ok = trace_buffer.Put(vptr[i]->GetValue());
 
     // Note that this can't fail as we've already checked for sufficient
     // space above.  I still need to check it though, becuase it's marked
@@ -100,47 +88,30 @@ void TraceSample() {
       break;
   }
 
-  traceSamp++;
+  return true;
 }
 
 // Fill the vptr array with pointers to all the active
 // trace variables and return the number of active ones
-int CountActiveVars(DebugVar *vptr[]) {
+int CountActiveVars(DebugVarBase *vptr[]) {
   int vct = 0;
 
   for (int i = 0; i < TRACE_VAR_CT; i++) {
-    if (traceVar[i] < 0)
+    if (trace_var[i] < 0)
       continue;
 
-    // Find the debug variable associated
-    // with this ID
-    vptr[vct] = DebugVar::FindVar(static_cast<uint16_t>(traceVar[i]));
+    // Find the debug variable associated with this ID
+    auto *var = DebugVar::FindVar(static_cast<uint16_t>(trace_var[i]));
 
     // If the ID wasn't valid, disable this trace variable
-    if (!vptr[vct])
-      traceVar[i] = -1;
-    else
+    if (!var) {
+      trace_var[i] = -1;
+    } else {
+      if (vptr != nullptr)
+        vptr[vct] = var;
       vct++;
+    }
   }
 
   return vct;
-}
-
-void TraceCtrlVar::SetValue(uint32_t value) {
-
-  // Temporarily replace the address with a local variable
-  // so the setting doesn't take effect immediately
-  // Then call the standard SetValue function
-  int32_t new_ctrl;
-  addr_ = &new_ctrl;
-
-  DebugVar::SetValue(value);
-
-  // When a new trace is started, flush the buffer first
-  if ((new_ctrl & 1) && !(traceCtrl & 1)) {
-    traceBuffer.Flush();
-    traceSamp = 0;
-  }
-  traceCtrl = new_ctrl;
-  addr_ = &traceCtrl;
 }

b94315372f2fd17ade8193b6222001b80bcec873
miceuz <mic@hardcore.lt>
2020-06-04 14:28:27 +0300

    Move serial event listeners from constructor of UART_DMA to particular startTX, startRX methods; some minor interface changes, some renaming

--- controller/lib/hal/uart_dma.cpp
+++ controller/lib/hal/uart_dma.cpp
@@ -1,9 +1,9 @@
 #if defined(BARE_STM32) && defined(UART_VIA_DMA)
 
 #include "uart_dma.h"
-#include "debug.h"
 #include "hal_stm32.h"
 #include "hal_stm32_regs.h"
+#include "units.h"
 
 // STM32 UART3 driver based on DMA transfers.
 
@@ -17,10 +17,11 @@
 // UART will issue an interrupt upon receipt of the specified
 // character.
 
-extern UART_DMA dmaUART;
+extern UART_DMA uart_dma;
 
 // Performs UART3 initialization
-void UART_DMA::init(int baud) {
+void UART_DMA::init(uint32_t baud) {
+  baud_ = baud;
   // Set baud rate register
   uart->baud = CPU_FREQ / baud;
 
@@ -51,13 +52,14 @@ void UART_DMA::init(int baud) {
   dma->channel[rxCh].config.tcie = 1;        // interrupt on DMA complete
 
   dma->channel[rxCh].config.mem2mem = 0; // memory-to-memory mode disabled
-  dma->channel[rxCh].config.msize = DmaTransferSize::BITS8;
-  dma->channel[rxCh].config.psize = DmaTransferSize::BITS8;
+  dma->channel[rxCh].config.msize = static_cast<REG>(DmaTransferSize::BITS8);
+  dma->channel[rxCh].config.psize = static_cast<REG>(DmaTransferSize::BITS8);
   dma->channel[rxCh].config.memInc = 1;   // increment destination (memory)
   dma->channel[rxCh].config.perInc = 0;   // don't increment source
                                           // (peripheral) address
   dma->channel[rxCh].config.circular = 0; // not circular
-  dma->channel[rxCh].config.dir = DmaChannelDir::PERIPHERAL_TO_MEM;
+  dma->channel[rxCh].config.dir =
+      static_cast<REG>(DmaChannelDir::PERIPHERAL_TO_MEM);
 
   dma->channel[txCh].config.priority = 0b11; // high priority
   dma->channel[txCh].config.teie = 1;        // interrupt on error
@@ -65,13 +67,14 @@ void UART_DMA::init(int baud) {
   dma->channel[txCh].config.tcie = 1;        // DMA complete interrupt enabled
 
   dma->channel[txCh].config.mem2mem = 0; // memory-to-memory mode disabled
-  dma->channel[txCh].config.msize = DmaTransferSize::BITS8;
-  dma->channel[txCh].config.psize = DmaTransferSize::BITS8;
+  dma->channel[txCh].config.msize = static_cast<REG>(DmaTransferSize::BITS8);
+  dma->channel[txCh].config.psize = static_cast<REG>(DmaTransferSize::BITS8);
   dma->channel[txCh].config.memInc = 1;   // increment source (memory) address
   dma->channel[txCh].config.perInc = 0;   // don't increment dest (peripheral)
                                           // address
   dma->channel[txCh].config.circular = 0; // not circular
-  dma->channel[txCh].config.dir = DmaChannelDir::MEM_TO_PERIPHERAL;
+  dma->channel[txCh].config.dir =
+      static_cast<REG>(DmaChannelDir::MEM_TO_PERIPHERAL);
 }
 
 // Sets up an interrupt on matching char incomming form UART3
@@ -96,16 +99,19 @@ bool UART_DMA::isRxInProgress() {
 // Returns false if DMA transmission is in progress, does not
 // interrupt previous transmission.
 // Returns true if no transmission is in progress
-bool UART_DMA::startTX(const char *buf, uint32_t length) {
+bool UART_DMA::startTX(const uint8_t *buf, uint32_t length, TxListener *txl) {
   if (isTxInProgress()) {
     return false;
   }
 
+  txListener = txl;
+
   dma->channel[txCh].config.enable = 0; // Disable channel before config
   // data sink
-  dma->channel[txCh].pAddr = reinterpret_cast<REG>(&(uart->txDat));
+  dma->channel[txCh].pAddr = (&(uart->txDat));
   // data source
-  dma->channel[txCh].mAddr = reinterpret_cast<REG>(buf);
+  dma->channel[txCh].mAddr =
+      const_cast<void *>(reinterpret_cast<const void *>(buf));
   // data length
   dma->channel[txCh].count = length & 0x0000FFFF;
 
@@ -125,32 +131,39 @@ void UART_DMA::stopTX() {
   }
 }
 
+// Converts Duration to the number of baudrate bits as that is used for rx
+// timeout. Max timeout supported by STM32 UART is 24 bit
+UART_DMA::uint24_t UART_DMA::DurationToBits(Duration d) {
+  return {0,
+          static_cast<uint32_t>(d.milliseconds() * baud_ / 1000) & 0x00FFFFFF};
+}
+
 // Sets up reception of at least [length] chars from UART3 into [buf]
-// [timeout] is the number of baudrate bits for which RX line is
-// allowed to be idle before asserting timeout error.
 // Returns false if reception is in progress, new reception is not
 // setup. Returns true if no reception is in progress and new reception
 // was setup.
 
-bool UART_DMA::startRX(const char *buf, const uint32_t length,
-                       const uint32_t timeout) {
+bool UART_DMA::startRX(const uint8_t *buf, uint32_t length, Duration timeout,
+                       RxListener *rxl) {
   // UART3 reception happens on DMA1 channel 3
   if (isRxInProgress()) {
     return false;
   }
 
+  rxListener = rxl;
+
   dma->channel[rxCh].config.enable = 0; // don't enable yet
 
   // data source
-  dma->channel[rxCh].pAddr = reinterpret_cast<REG>(&(uart->rxDat));
+  dma->channel[rxCh].pAddr = &(uart->rxDat);
   // data sink
-  dma->channel[rxCh].mAddr = reinterpret_cast<REG>(buf);
+  dma->channel[rxCh].mAddr =
+      const_cast<void *>(reinterpret_cast<const void *>(buf));
   // data length
   dma->channel[rxCh].count = length;
 
   // setup rx timeout
-  // max timeout is 24 bit
-  uart->timeout.s.rto = timeout & 0x00FFFFFF;
+  uart->timeout.s.rto = DurationToBits(timeout).bits;
   uart->intClear.s.rtocf = 1; // Clear rx timeout flag
   uart->request.s.rxfrq = 1;  // Clear RXNE flag
   uart->ctrl1.s.rtoie = 1;    // Enable receive timeout interrupt
@@ -201,7 +214,7 @@ void UART_DMA::UART_ISR() {
       e = RxError_t::RX_ERROR_OVR;
     }
     if (uart->status.s.fe) {
-      e = RxError_t::RX_ERROR_FRAMING;
+      e = RxError_t::RX_ERROR_SERIAL_FRAMING;
     }
 
     uart->request.s.rxfrq = 1; // Clear RXNE flag before clearing other flags
@@ -212,50 +225,62 @@ void UART_DMA::UART_ISR() {
     uart->intClear.s.rtocf = 1;
 
     // TODO define logic if stopRX() has to be here
-    rxListener.onRxError(e);
+    if (rxListener) {
+      rxListener->onRxError(e);
+    }
   }
 
   if (isCharacterMatchInterrupt()) {
     uart->request.s.rxfrq = 1; // Clear RXNE flag before clearing other flags
     uart->intClear.s.cmcf = 1; // Clear char match flag
     // TODO define logic if stopRX() has to be here
-    rxListener.onCharacterMatch();
+    if (rxListener) {
+      rxListener->onCharacterMatch();
+    }
   }
 }
 
 void UART_DMA::DMA_TX_ISR() {
   if (dma->intStat.teif2) {
     stopTX();
-    txListener.onTxError();
+    if (txListener) {
+      txListener->onTxError();
+    }
   } else {
     stopTX();
-    txListener.onTxComplete();
+    if (txListener) {
+      txListener->onTxComplete();
+    }
   }
 }
 
 void UART_DMA::DMA_RX_ISR() {
   if (dma->intStat.teif3) {
     stopRX();
-    rxListener.onRxError(RxError_t::RX_ERROR_DMA);
+    if (rxListener) {
+      rxListener->onRxError(RxError_t::RX_ERROR_DMA);
+    }
   } else {
     stopRX();
-    rxListener.onRxComplete();
+    if (rxListener) {
+      rxListener->onRxComplete();
+    }
   }
 }
 
 void DMA1_CH2_ISR() {
   DMA_Regs *dma = DMA1_BASE;
-  dmaUART.DMA_TX_ISR();
+  uart_dma.DMA_TX_ISR();
   dma->intClr.gif2 = 1; // clear all channel 3 flags
 }
 
 void DMA1_CH3_ISR() {
   DMA_Regs *dma = DMA1_BASE;
-  dmaUART.DMA_RX_ISR();
+  uart_dma.DMA_RX_ISR();
   dma->intClr.gif3 = 1; // clear all channel 2 flags
 }
 
 // This is the interrupt handler for the UART.
-void UART3_ISR() { dmaUART.UART_ISR(); }
+void UART3_ISR() { uart_dma.UART_ISR(); }
 
 #endif

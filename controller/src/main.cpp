386f82e8db465920735b67ba98605fa7fb9c4fb9
Steve Glow <sglow@embeddedintelligence.com>
2020-06-07 10:16:06 -0400

    Default mode should be off, even for debugging

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -40,9 +40,8 @@ limitations under the License.
 //
 // "Sends" data to the "GUI" via a simple serial protocol.  This can be parsed
 // and graphed by e.g. the Arduino IDE (tools -> serial plotter).
-static DebugUInt32
-    gui_mode("gui_mode", "Mode setting for GUI free testing",
-             static_cast<uint32_t>(VentMode::VentMode_PRESSURE_CONTROL));
+static DebugUInt32 gui_mode("gui_mode", "Mode setting for GUI free testing",
+                            static_cast<uint32_t>(VentMode::VentMode_OFF));
 static DebugUInt32 gui_bpm("gui_bpm", "Breaths/min for GUI free testing", 15);
 static DebugUInt32 gui_peep("gui_peep", "PEEP (cm/h2O) for GUI free testing",
                             5);

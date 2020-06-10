c7be2b67ce3b0f63d48c722e5e3051222355ba4e
miceuz <mic@hardcore.lt>
2020-06-10 11:01:20 +0300

    Fix header guards

--- software/common/libs/framing/framing_spec_chars.h
+++ software/common/libs/framing/framing_spec_chars.h
@@ -1,5 +1,5 @@
-#ifndef __FRAMING_SPEC_CHARS_H__
-#define __FRAMING_SPEC_CHARS_H__
+#ifndef FRAMING_SPEC_CHARS_H_
+#define FRAMING_SPEC_CHARS_H_
 #include <stdint.h>
 constexpr uint8_t FRAMING_MARK = 0xE2;
 constexpr uint8_t FRAMING_ESC = 0x27;

31b3f491e8f03d300cb2091eab8d9be4e367435a
Justin Lebar <justin.lebar@gmail.com>
2020-06-11 16:22:24 -0700

    Move volume sensing into controller.
    At long last.
    
    This fixes a layering violation (sensors module shouldn't care about
    breaths), and opens the door to more sophisticated volume zeroing logic.
    It's also easier to test, because you don't have to compute voltages!
    
    I had to fix an edge case in blower_fsm; our breaths were one
    microsecond too long.  Not a big deal except in tests.
    
    GPC: volume-sensing-in-controller

--- controller/lib/core/controller.cpp
+++ controller/lib/core/controller.cpp
@@ -21,73 +21,113 @@ limitations under the License.
 
 static constexpr Duration LOOP_PERIOD = milliseconds(10);
 
-static DebugFloat dbg_setpoint("setpoint", "Setpoint pressure, kPa");
+// Inputs - set from external debug program, read but never modified here.
+static DebugFloat dbg_blower_valve_kp("blower_valve_kp",
+                                      "Proportional gain for blower valve PID",
+                                      0.7f);
+static DebugFloat dbg_blower_valve_ki("blower_valve_ki",
+                                      "Integral gain for blower valve PID",
+                                      1.0f);
+static DebugFloat dbg_blower_valve_kd("blower_valve_kd",
+                                      "Derivative gain for blower valve PID");
 
-static DebugFloat dbg_kp("kp", "Proportional gain for main loop", 0.7f);
-static DebugFloat dbg_ki("ki", "Integral gain for main loop", 1.0f);
-static DebugFloat dbg_kd("kd", "Derivative gain for main loop");
-static DebugFloat dbg_sp("pc_setpoint", "Pressure control setpoint (cmH2O)");
-static DebugUInt32 dbg_per("loop_period", "Loop period (usec)",
+// Unchanging outputs - read from external debug program, never modified here.
+static DebugUInt32 dbg_per("loop_period", "Loop period, read-only (usec)",
                            static_cast<uint32_t>(LOOP_PERIOD.microseconds()));
 
+// Outputs - read from external debug program, modified here.
+static DebugFloat dbg_sp("pc_setpoint", "Pressure control setpoint (cmH2O)");
+static DebugFloat dbg_net_flow("net_flow", "Net flow rate, cc/sec");
+static DebugFloat dbg_volume("volume", "Patient volume, ml");
+static DebugFloat
+    dbg_net_flow_uncorrected("net_flow_uncorrected",
+                             "Net flow rate w/o correction, cc/sec");
+static DebugFloat dbg_volume_uncorrected("uncorrected_volume",
+                                         "Patient volume w/o correction, ml");
+
 Controller::Controller()
-    : pid_(dbg_kp.Get(), dbg_ki.Get(), dbg_kd.Get(), ProportionalTerm::ON_ERROR,
-           DifferentialTerm::ON_MEASUREMENT, /*output_min=*/0.f,
-           /*output_max=*/1.0f) {}
+    : blower_valve_pid_(dbg_blower_valve_kp.Get(), dbg_blower_valve_ki.Get(),
+                        dbg_blower_valve_kd.Get(), ProportionalTerm::ON_ERROR,
+                        DifferentialTerm::ON_MEASUREMENT, /*output_min=*/0.f,
+                        /*output_max=*/1.0f) {}
 
 /*static*/ Duration Controller::GetLoopPeriod() { return LOOP_PERIOD; }
 
 std::pair<ActuatorsState, ControllerState>
 Controller::Run(Time now, const VentParams &params,
                 const SensorReadings &sensor_readings) {
-  BlowerSystemState desired_state =
-      fsm_.DesiredState(now, params, sensor_readings);
+  blower_valve_pid_.SetKP(dbg_blower_valve_kp.Get());
+  blower_valve_pid_.SetKI(dbg_blower_valve_ki.Get());
+  blower_valve_pid_.SetKD(dbg_blower_valve_kd.Get());
 
-  if (desired_state.pressure_setpoint == std::nullopt) {
-    pid_.Reset();
+  VolumetricFlow uncorrected_net_flow =
+      sensor_readings.inflow - sensor_readings.outflow;
+  flow_integrator_->AddFlow(now, uncorrected_net_flow);
+  uncorrected_flow_integrator_->AddFlow(now, uncorrected_net_flow);
+
+  Volume patient_volume = flow_integrator_->GetTV();
+  VolumetricFlow net_flow =
+      uncorrected_net_flow + flow_integrator_->FlowCorrection();
+
+  BlowerSystemState desired_state = fsm_.DesiredState(
+      now, params, {.patient_volume = patient_volume, .net_flow = net_flow});
+
+  if (desired_state.is_new_breath) {
+    // The "correct" volume at the breath boundary is 0.
+    flow_integrator_->NoteExpectedVolume(ml(0));
   }
 
-  pid_.SetKP(dbg_kp.Get());
-  pid_.SetKI(dbg_ki.Get());
-  pid_.SetKD(dbg_kd.Get());
+  ActuatorsState actuators_state;
+  if (desired_state.pressure_setpoint == std::nullopt) {
+    // System disabled.  Disable blower, close inspiratory pinch valve, and
+    // open expiratory pinch valve.  This way if someone is hooked up, they can
+    // breathe through the expiratory branch, and they can't contaminate the
+    // inspiratory branch.
+    //
+    // If the pinch valves are not yet homed, this will home them and then move
+    // them to the desired positions.
+    blower_valve_pid_.Reset();
 
-  float pressure = sensor_readings.patient_pressure.kPa();
-  dbg_sp.Set(desired_state.pressure_setpoint.value_or(kPa(0)).cmH2O());
+    // Reset the two flow integrators, forcing volume to 0.
+    //
+    // TODO: This isn't ideal; if the blower is off, we should still be able to
+    // report volume.
+    flow_integrator_.emplace();
+    uncorrected_flow_integrator_.emplace();
 
-  ControllerState controller_state = {
-      .is_new_breath = desired_state.is_new_breath,
-      .pressure_setpoint = desired_state.pressure_setpoint.value_or(kPa(0)),
-  };
-
-  ActuatorsState actuators_state = [&]() -> ActuatorsState {
-    if (desired_state.pressure_setpoint == std::nullopt) {
-      // System disabled.  Disable blower, close inspiratory pinch valve, and
-      // open expiratory pinch valve.  This way if someone is hooked up, they
-      // can breathe through the expiratory branch, and they can't contaminate
-      // the inspiratory branch.
-      //
-      // If the pinch valves are not yet homed, this will home them and then
-      // move them to the desired positions.
-      return {
-          .fio2_valve = 0,
-          .blower_power = 0,
-          .blower_valve = 0,
-          .exhale_valve = 1,
-      };
-    }
-
+    actuators_state = {
+        .fio2_valve = 0,
+        .blower_power = 0,
+        .blower_valve = 0,
+        .exhale_valve = 1,
+    };
+  } else {
     // Start controlling pressure.
-    return {
+    actuators_state = {
         .fio2_valve = 0, // not used yet
         // In normal mode, blower is always full power; pid controls pressure by
         // actuating the blower pinch valve.
         .blower_power = 1,
-        .blower_valve =
-            pid_.Compute(now, pressure, desired_state.pressure_setpoint->kPa()),
+        .blower_valve = blower_valve_pid_.Compute(
+            now, sensor_readings.patient_pressure.kPa(),
+            desired_state.pressure_setpoint->kPa()),
         .exhale_valve =
             desired_state.flow_direction == FlowDirection::EXPIRATORY ? 1 : 0,
     };
-  }();
+  }
+
+  ControllerState controller_state = {
+      .pressure_setpoint = desired_state.pressure_setpoint.value_or(kPa(0)),
+      .patient_volume = patient_volume,
+      .net_flow = net_flow,
+  };
+
+  dbg_sp.Set(desired_state.pressure_setpoint.value_or(kPa(0)).cmH2O());
+  dbg_net_flow.Set(controller_state.net_flow.ml_per_sec());
+  dbg_net_flow_uncorrected.Set(
+      (sensor_readings.inflow - sensor_readings.outflow).ml_per_sec());
+  dbg_volume.Set(controller_state.patient_volume.ml());
+  dbg_volume_uncorrected.Set(uncorrected_flow_integrator_->GetTV().ml());
 
   return {actuators_state, controller_state};
 }

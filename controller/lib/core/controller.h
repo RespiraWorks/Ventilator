07ca49278e16a9dabcc4d4e94b14a2e9b04111d3
Justin Lebar <justin.lebar@gmail.com>
2020-06-11 16:22:24 -0700

    Move volume sensing into controller.
    At long last.
    
    This fixes a layering violation (sensors module shouldn't care about
    breaths), and opens the door to more sophisticated volume zeroing logic.
    It's also easier to test, because you don't have to compute voltages!
    
    I had to fix an edge case in blower_fsm; our breaths were one
    microsecond too long.  Not a big deal except in tests.
    
    GPC: volume-sensing-in-controller

--- controller/lib/core/controller.h
+++ controller/lib/core/controller.h
@@ -3,18 +3,28 @@
 
 #include "actuators.h"
 #include "blower_fsm.h"
+#include "flow_integrator.h"
 #include "network_protocol.pb.h"
 #include "pid.h"
 #include "sensors.h"
 #include "units.h"
+#include <optional>
 #include <utility>
 
+// TODO: This name is too close to the ControllerStatus proto.
 struct ControllerState {
-  // True if this is the first breath of a new cycle.
-  bool is_new_breath;
-
   // Patient pressure the controller wants to achieve.
   Pressure pressure_setpoint;
+
+  // Patient volume and net flow (inflow minus outflow) are properties of
+  // ControllerState, not SensorReadings, because flow and volume measurement
+  // require error corrections that can only happen at the controller level.
+  //
+  // The "correct" volume after a normal breath is 0, and it's the controller
+  // that knows when breaths occur.  We use this info to drive flow (and
+  // therefore volume) to 0 at each breath boundary.
+  Volume patient_volume;
+  VolumetricFlow net_flow;
 };
 
 // This class is here to allow integration of our controller into Modelica
@@ -31,7 +41,17 @@ public:
 
 private:
   BlowerFsm fsm_;
-  PID pid_;
+  PID blower_valve_pid_;
+
+  // These objects accumulate flow to calculate volume.
+  //
+  // For debugging, we accumulate flow with and without error correction.  See
+  // FlowIntegrator definition for description of errors.
+  //
+  // These are never nullopt; we use std::optional to let us clear/reset these
+  // objects.
+  std::optional<FlowIntegrator> flow_integrator_ = FlowIntegrator();
+  std::optional<FlowIntegrator> uncorrected_flow_integrator_ = FlowIntegrator();
 };
 
 #endif // CONTROLLER_H_

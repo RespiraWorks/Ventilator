7aebd0506014133451e698a6b9d05b8e0719e2cb
Steve Glow <sglow@embeddedintelligence.com>
2020-06-10 20:12:36 -0400

    Comment changes

--- controller/lib/hal/buzzer.cpp
+++ controller/lib/hal/buzzer.cpp
@@ -17,6 +17,9 @@ limitations under the License.
 // The buzzer we use for generating alarms on the controller board is
 // part number CT-1205H-SMT-TR made by CUI.
 //
+// The data sheet for this part can be found here:
+// https://www.cuidevices.com/product/resource/ct-1205h-smt-tr.pdf
+//
 // This part is designed to be driven with a 2.4kHz square wave.
 //
 // On the controller board, pin PB4 is used to actuate the buzzer.
@@ -33,6 +36,9 @@ void HalApi::InitBuzzer() {
   EnableClock(TIMER3_BASE);
 
   // Connect PB4 to timer 3
+  // The STM32 datasheet has a table (table 17) which shows
+  // which functions can be connected to each pin.  For
+  // PB4 we select function 2 to connect it to timer 3.
   GPIO_PinAltFunc(GPIO_B_BASE, 4, 2);
 
   TimerRegs *tmr = TIMER3_BASE;
@@ -45,6 +51,10 @@ void HalApi::InitBuzzer() {
   // the new PWM duty cycle gets written to a shadow
   // register and copied to the active register
   // at the start of the next cycle.
+  //
+  // TODO - the ccMode and ccEnable registers of the timer
+  // should really be converted to bit flags for better
+  // readability
   tmr->ccMode[0] = 0x0068;
 
   tmr->ccEnable = 0x01;

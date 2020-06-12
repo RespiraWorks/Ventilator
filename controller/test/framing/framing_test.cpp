84ae8c9829270a619c7b5cfdf72b3ebd734430be
miceuz <mic@hardcore.lt>
2020-06-12 14:13:47 +0300

    Catch up with renames

--- controller/test/framing/framing_test.cpp
+++ controller/test/framing/framing_test.cpp
@@ -51,7 +51,7 @@ bool UART_DMA::startTX(const uint8_t *buf, uint32_t length, TxListener *txl) {
 
 void UART_DMA::DMA_TX_ISR() {
   is_txing = false;
-  tx_listener->onTxComplete();
+  tx_listener->OnTxComplete();
 }
 
 void UartDmaReset() { uart_dma.DMA_TX_ISR(); }

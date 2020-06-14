80d13a6e74c8082bb7c455b64c13cc0f192632b4
Eugene Kirpichov <ekirpichov@gmail.com>
2020-06-13 18:47:25 -0700

    Adds the first alarm and tests

--- gui/tests/tst_main.cpp
+++ gui/tests/tst_main.cpp
@@ -1,21 +1,16 @@
 #include <QCoreApplication>
 #include <QtTest>
 
-#include "goodbyetest.h"
-#include "hellotest.h"
+#include "max_pressure_alarm_test.h"
 
 int main(int argc, char *argv[]) {
   QGuiApplication app(argc, argv);
 
   int status = 0;
   {
-    HelloTest tc;
+    MaxPressureAlarmTest tc;
     status = QTest::qExec(&tc, argc, argv);
   }
 
-  {
-    GoodbyeTest tc;
-    status |= QTest::qExec(&tc, argc, argv);
-  }
   return status;
 }

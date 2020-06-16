c67a5881a5151d885efbd2b80fbca7e10c7796fa
Eugene Kirpichov <ekirpichov@gmail.com>
2020-06-15 22:03:28 -0700

    Measures recent-breath PIP, PEEP, RR, IER

--- gui/tests/tst_main.cpp
+++ gui/tests/tst_main.cpp
@@ -1,6 +1,7 @@
 #include <QCoreApplication>
 #include <QtTest>
 
+#include "breath_signals_test.h"
 #include "max_pressure_alarm_test.h"
 
 int main(int argc, char *argv[]) {
@@ -9,7 +10,12 @@ int main(int argc, char *argv[]) {
   int status = 0;
   {
     MaxPressureAlarmTest tc;
-    status = QTest::qExec(&tc, argc, argv);
+    status += QTest::qExec(&tc, argc, argv);
+  }
+
+  {
+    BreathSignalsTest tc;
+    status += QTest::qExec(&tc, argc, argv);
   }
 
   return status;

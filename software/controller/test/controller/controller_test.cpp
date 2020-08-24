cdcd8908228dd0a0b3c343c9e728475d35800e25
asmodai27 <asmodai27@wanadoo.fr>
2020-08-24 23:49:12 +0200

    Improve lib/core/controller test coverage with test of pressure control laws

--- software/controller/test/controller/controller_test.cpp
+++ software/controller/test/controller/controller_test.cpp
@@ -119,3 +119,123 @@ TEST(ControllerTest, BreathId) {
     EXPECT_EQ(breath_start.microsSinceStartup(), status.breath_id);
   }
 }
+
+// Checks that a sequence of calls to controller_run() yields the expected
+// actuators state
+void actuatorsTestSequence(
+    const std::vector<std::tuple<
+        VentParams, SensorReadings, /*time_millis*/ uint64_t,
+        /*expected_blower_power*/ float, /*expected_oxy_valve*/ float,
+        /*expected_air_vave*/ float, /*expected_exhale_valve*/ float>> &seq) {
+  if (seq.empty()) {
+    return;
+  }
+  constexpr float kValveStateTolerance{.001f};
+
+  // Reset time to test's start time.
+  Hal.delay(microseconds(-Hal.now().microsSinceStartup()));
+  Hal.delay(milliseconds(std::get<uint64_t>(seq.front())));
+
+  Controller c;
+  VentParams last_params = VentParams_init_zero;
+  SensorReadings last_readings = {
+      .patient_pressure = cmH2O(0),
+      .inflow_pressure_diff = kPa(0),
+      .outflow_pressure_diff = kPa(0),
+      .fio2 = 0,
+      .inflow = ml_per_min(0),
+      .outflow = ml_per_min(0),
+  };
+
+  for (const auto &[params, readings, time_millis, blower, psol, air_valve,
+                    exhale_valve] : seq) {
+
+    SCOPED_TRACE("time = " + std::to_string(time_millis));
+    // Move time forward to t in steps of Controller::GetLoopPeriod().
+    Time t = microsSinceStartup(1000 * time_millis);
+    while (Hal.now() < t) {
+      Hal.delay(Controller::GetLoopPeriod());
+      (void)c.Run(Hal.now(), last_params, last_readings);
+    }
+    EXPECT_EQ(t.microsSinceStartup(), Hal.now().microsSinceStartup());
+
+    auto [act_state, unused_status] = c.Run(Hal.now(), params, readings);
+    (void)unused_status;
+
+    EXPECT_FLOAT_EQ(act_state.blower_power, blower);
+    EXPECT_NEAR(act_state.fio2_valve, psol, kValveStateTolerance);
+    EXPECT_NEAR(act_state.blower_valve.value(), air_valve,
+                kValveStateTolerance);
+    EXPECT_NEAR(act_state.exhale_valve.value(), exhale_valve,
+                kValveStateTolerance);
+    last_params = params;
+    last_readings = readings;
+  }
+}
+
+TEST(ControllerTest, ControlLaws) {
+
+  // typical expected values
+  constexpr float kBlowerOn{1.0f};
+  constexpr float kBlowerOff{0};
+  constexpr float kValveOpen{1.0f};
+  constexpr float kAlmostClosed{0.05f};
+  constexpr float kValveClosed{0};
+  constexpr float kExhaleMaxOpen{0.6f};
+
+  VentParams params = VentParams_init_zero;
+  params.mode = VentMode::VentMode_PRESSURE_CONTROL;
+  // Setting PIP=PEEP in order not to care about respiratory cycle
+  params.peep_cm_h2o = 50;
+  params.pip_cm_h2o = params.peep_cm_h2o;
+  params.breaths_per_min = 15;
+  params.inspiratory_expiratory_ratio = 1;
+  params.fio2 = 0.21f;
+
+  // Readings with patient pressure == Setpoint, should lead to inlet valve
+  // almost closed and exhale valve in its max openness state
+  SensorReadings readings_pip = {
+      .patient_pressure = cmH2O(static_cast<float>(params.pip_cm_h2o)),
+      .inflow_pressure_diff = kPa(0),
+      .outflow_pressure_diff = kPa(0),
+      .fio2 = 0.21f,
+      .inflow = ml_per_min(0),
+      .outflow = ml_per_min(0),
+  };
+
+  // Readings with patient pressure << Setpoint, should lead to inlet valve
+  // fully open and exhale valve either almost closed (when using air) or fully
+  // closed (when using oxygen, in order not to waste oxygen)
+  SensorReadings readings_below_pip = {
+      .patient_pressure = cmH2O(-500),
+      .inflow_pressure_diff = kPa(0),
+      .outflow_pressure_diff = kPa(0),
+      .fio2 = 0.21f,
+      .inflow = ml_per_min(0),
+      .outflow = ml_per_min(0),
+  };
+
+  // pressure control pure air test
+  actuatorsTestSequence(
+      {{params, readings_pip, 0, /*blower=*/kBlowerOn, /*psol=*/kValveClosed,
+        /*air_valve=*/kAlmostClosed, kExhaleMaxOpen},
+       {params, readings_below_pip, 1000, /*blower=*/kBlowerOn,
+        /*psol=*/kValveClosed,
+        /*air_valve=*/kValveOpen, /*exhale=*/kAlmostClosed}});
+
+  // pressure control oxygen test: pp = PEEP == PIP
+  params.fio2 = 1.0f;
+  actuatorsTestSequence(
+      {{params, readings_pip, 0, /*blower=*/kBlowerOff,
+        /*psol=*/kAlmostClosed,
+        /*air_valve=*/kValveClosed, kExhaleMaxOpen},
+       {params, readings_below_pip, 1000,
+        /*blower=*/kBlowerOff, /*psol=*/kValveOpen,
+        /*air_valve=*/kValveClosed, /*exhale=*/kValveClosed}});
+
+  // Off mode: blower is off, inhale valves are closed, exhale valve is open
+  params.mode = VentMode::VentMode_OFF;
+  actuatorsTestSequence(
+      {{params, readings_pip, 0, /*blower=*/kBlowerOff, /*psol=*/kValveClosed,
+        /*air_valve=*/kValveClosed, /*exhale=*/kValveOpen}});
+}

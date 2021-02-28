da77339999c172bc6a58deacaf656732c6d692c7
Daniel Heater <daniel@heatertech.com>
2021-02-28 13:45:50 -0600

    format

--- software/controller/test/psol/psol_test.cpp
+++ software/controller/test/psol/psol_test.cpp
@@ -13,8 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include "gtest/gtest.h"
 #include "hal.h"
+#include "gtest/gtest.h"
 
 // Not really a test, just silencing code coverage warning for native build
 TEST(PSol, TestStubs) { PSOL_Value(0.0); }

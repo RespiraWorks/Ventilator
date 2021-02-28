67d0246ef03f26f649e4e934cc524a7e8251afc4
Daniel Heater <daniel@heatertech.com>
2021-02-28 13:52:30 -0600

    format

--- software/controller/test/stepper/stepper_test.cpp
+++ software/controller/test/stepper/stepper_test.cpp
@@ -13,8 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include "gtest/gtest.h"
 #include "stepper.h"
+#include "gtest/gtest.h"
 
 // Not really tests, just silencing code coverage warning for native build
 TEST(Stepper, TestStubs) {

aada9d3c41ec3e27caa0c3b1690637b9135cc564
Martin Shetty <1972005+martukas@users.noreply.github.com>
2021-09-14 14:39:31 -0700

    "using gtest_print; updates #1167"

--- software/common/test_libs/gtest_print.h
+++ software/common/test_libs/gtest_print.h
@@ -19,14 +19,13 @@ limitations under the License.
 
 namespace testing {
 namespace internal {
-enum GTestColor { COLOR_DEFAULT, COLOR_RED, COLOR_GREEN, COLOR_YELLOW };
 
 extern void ColoredPrintf(GTestColor color, const char* fmt, ...);
 }  // namespace internal
 }  // namespace testing
 #define PRINTF(...)                                                                    \
   do {                                                                                 \
-    testing::internal::ColoredPrintf(testing::internal::COLOR_GREEN, "[          ] "); \
+    testing::internal::ColoredPrintf(testing::internal::COLOR_GREEN, "[   INFO   ] "); \
     testing::internal::ColoredPrintf(testing::internal::COLOR_YELLOW, __VA_ARGS__);    \
   } while (0)
 

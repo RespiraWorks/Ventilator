78dc4145f09df417af4c697d03b11897038248c6
Martin Shetty <1972005+martukas@users.noreply.github.com>
2021-09-30 02:38:14 -0700

    "simplified and streamlined CMake config; updates #1180"

--- software/gui/cmake/QtMacFix.cmake
+++ software/gui/cmake/QtMacFix.cmake
@@ -4,11 +4,3 @@ if(APPLE AND EXISTS /usr/local/opt/qt)
   # it is not in the default /usr/local prefix.
   list(APPEND CMAKE_PREFIX_PATH "/usr/local/opt/qt")
 endif()
-
-set(CMAKE_INCLUDE_CURRENT_DIR ON)
-set(CMAKE_AUTOMOC ON)
-
-find_package(Qt5 COMPONENTS
-  Core Quick SerialPort Qml Multimedia Test
-  OpenGL OpenGLExtensions
-  REQUIRED)

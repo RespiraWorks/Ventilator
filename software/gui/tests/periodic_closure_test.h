c019efa2056071ad6b6a9d8a1f511ad0dae0de28
Martin Shetty <1972005+martukas@users.noreply.github.com>
2021-10-03 02:51:46 -0700

    "test stubs for most gui classes; updates #842"

---
+++ software/gui/tests/periodic_closure_test.h
@@ -0,0 +1,36 @@
+/* Copyright 2020-2021, RespiraWorks
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+#include <QCoreApplication>
+#include <QtTest>
+
+#include "periodic_closure.h"
+
+class PeriodicClosureTest : public QObject {
+  Q_OBJECT
+ public:
+  PeriodicClosureTest() = default;
+  ~PeriodicClosureTest() = default;
+
+ private slots:
+  void initTestCase() {}
+  void cleanupTestCase() {}
+
+  void testInit() {
+    PeriodicClosure pc(DurationMs(0), [] {});
+  }
+};

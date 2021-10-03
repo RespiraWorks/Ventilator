e18f8fcfc7f74d0ce3ae87660a2a206ea123375c
Martin Shetty <1972005+martukas@users.noreply.github.com>
2021-10-03 02:51:46 -0700

    "test stubs for most gui classes; updates #842"

--- software/gui/tests/tst_main.cpp
+++ software/gui/tests/tst_main.cpp
@@ -16,10 +16,20 @@ limitations under the License.
 #include <QCoreApplication>
 #include <QtTest>
 
+#include "alarm_manager_test.h"
 #include "breath_signals_test.h"
+#include "controller_history_test.h"
+#include "gui_state_container_test.h"
 #include "latching_alarm_test.h"
 #include "logger_test.h"
 #include "patient_detached_alarm_test.h"
+#include "periodic_closure_test.h"
+#include "pip_exceeded_alarm_test.h"
+#include "pip_not_reached_alarm_test.h"
+#include "respira_connected_device_test.h"
+#include "simple_clock_test.h"
+#include "time_series_graph_painter_test.h"
+#include "time_series_graph_test.h"
 
 int main(int argc, char *argv[]) {
   QGuiApplication app(argc, argv);
@@ -27,12 +37,7 @@ int main(int argc, char *argv[]) {
   int status = 0;
 
   {
-    LoggerTest tc;
-    status += QTest::qExec(&tc, argc, argv);
-  }
-
-  {
-    LatchingAlarmTest tc;
+    AlarmManagerTest tc;
     status += QTest::qExec(&tc, argc, argv);
   }
 
@@ -42,7 +47,62 @@ int main(int argc, char *argv[]) {
   }
 
   {
+    ControllerHistoryTest tc;
+    status += QTest::qExec(&tc, argc, argv);
+  }
+
+  {
+    GuiStateContainerTest tc;
+    status += QTest::qExec(&tc, argc, argv);
+  }
+
+  {
+    LatchingAlarmTest tc;
+    status += QTest::qExec(&tc, argc, argv);
+  }
+
+  {
+    LoggerTest tc;
+    status += QTest::qExec(&tc, argc, argv);
+  }
+
+  {
     PatientDetachedAlarmTest tc;
+    status += QTest::qExec(&tc, argc, argv);
+  }
+
+  {
+    PeriodicClosureTest tc;
+    status += QTest::qExec(&tc, argc, argv);
+  }
+
+  {
+    PipExceededAlarmTest tc;
+    status += QTest::qExec(&tc, argc, argv);
+  }
+
+  {
+    PipNotReachedAlarmTest tc;
+    status += QTest::qExec(&tc, argc, argv);
+  }
+
+  {
+    RespiraConnectedDeviceTest tc;
+    status += QTest::qExec(&tc, argc, argv);
+  }
+
+  {
+    SimpleClockTest tc;
+    status += QTest::qExec(&tc, argc, argv);
+  }
+
+  //  {
+  //    TimeSeriesGraphPainterTest tc;
+  //    status += QTest::qExec(&tc, argc, argv);
+  //  }
+
+  {
+    TimeSeriesGraphTest tc;
     status += QTest::qExec(&tc, argc, argv);
   }
 

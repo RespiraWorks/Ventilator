e9145a56881bc2b0f4f73863c65aebd0505ccb30
Martin Shetty <1972005+martukas@users.noreply.github.com>
2021-10-07 23:36:51 -0700

    "small updates; updates #1180"

--- software/utils/rpi_config/README.md
+++ software/utils/rpi_config/README.md
@@ -1,26 +1,24 @@
 # Automatic deployment on integrated prototypes
 
-The following scripts and configurations are here to help alleviate the frustration of having to perform the many
-arcane incantations to get a prototype running with software.
+The following scripts and configurations are here to help alleviate the frustration of having to perform the many arcane incantations to get a prototype running with software.
 
-**DISCLAIMER: You should not use this on any living beings - as a ventilator, or in any other way.
-These are utilities for bench with mechanical lung simulators only.
-It is also assumed that you are familiar with the software architecture and generally know what you are doing.**
+**DISCLAIMER: You should not use this on any living beings - as a ventilator, or in any other way. These are utilities for bench with mechanical lung simulators only. It is also assumed that you are familiar with the software architecture and generally know what you are doing.**
 
-The following assumes that you have a working hardware prototype with all necessary electronics, correctly wired for
-power and communication. See the [manufacturing](../../../manufacturing) section for details.
+The following assumes that you have a working hardware prototype with all necessary electronics, correctly wired for power and communication. See the [manufacturing](../../../manufacturing) section for details.
 
 ## How to set up
 
-* Start with a clean SD card of about 32GB. Building the software can be a resource-greedy process.
-* Prepare an SD card with a vanilla Raspberry Pi OS, the full desktop variety.
+* Start with a clean SD card of at least 32GB. Building the software can be a resource-greedy process.
+* Prepare an SD card with a [Ubuntu MATE](https://ubuntu-mate.org/download/arm64/focal/) distribution. Scripts have been tested with the `20.04.3 Focal Fossa`.
 * Make sure you have a USB connection from Raspi to Nucleo if you intend to flash firmware locally
-* Boot up and go through the standard Raspi configuration steps - locale, Wifi, update
+* Boot up and go through the standard Raspi configuration steps - locale, Wifi, etc..
+  * set up your username to be`respira`
+  * allow it to boot without login (recommended)
 * Reboot
 
 Now, open up a browser, and come back to this page.
 
-Just copy and paste this dangerous script into the console:
+Just copy and paste this script into a terminal:
 
 # TODO: change command to point to master
 
--- software/utils/rpi_config/bootstrap.sh
+++ software/utils/rpi_config/bootstrap.sh
@@ -14,39 +14,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 # Fail if any command fails
 set -e
 set -o pipefail
 
-echo "==============================================================================="
-echo "=================== RespiraWorks Ventilator bootstrapper ======================"
-echo "==============================================================================="
-echo " "
-echo "  -- clones the repository"
-echo "  -- configures Raspberry Pi:"
-echo "     * no screensaver or screen blanking"
-echo "     * no splash on bootup"
-echo "     * serial interface enabled for GUI<->controller communications"
-echo "  -- udev rule added for nucleo, enabling flashing of firmware"
-echo "  -- guake console accessible at all times using [F12]"
-echo "  -- desktop shortcuts for ventilator GUI, updater, debug interface"
-echo "  -- RW-theme background"
-echo "  -- installs toolchains and dependencies for building controller and GUI"
-echo "  -- will require a restart when done"
-echo " "
-echo " Will require a restart when complete."
-read -n1 -s -r -p $'Press any key to continue...\n' key
-
 # Print each command as it executes
-# TODO only if verbose
-set -o xtrace
+if [ -n "$VERBOSE" ]; then
+  set -o xtrace
+fi
+
+EXIT_FAILURE=1
+EXIT_SUCCESS=0
 
 # Check if Linux
 PLATFORM="$(uname -s)"
 if [ $PLATFORM != "Linux" ]; then
   echo "Error: This script only supports 'Linux'. You have $PLATFORM."
-  exit 1
+  exit $EXIT_FAILURE
+fi
+
+if [ -z "$VERBOSE" ]; then
+  echo "==============================================================================="
+  echo "=================== RespiraWorks Ventilator bootstrapper ======================"
+  echo "==============================================================================="
+  echo " "
+  echo "  -- clones the repository"
+  echo "  -- configures Raspberry Pi:"
+  echo "     * no screensaver or screen blanking"
+  echo "     * no splash on bootup"
+  echo "     * serial interface enabled for GUI<->controller communications"
+  echo "  -- udev rule added for nucleo, enabling flashing of firmware"
+  echo "  -- guake console accessible at all times using [F12]"
+  echo "  -- desktop shortcuts for ventilator GUI, updater, debug interface"
+  echo "  -- RW-theme background"
+  echo "  -- installs toolchains and dependencies for building controller and GUI"
+  echo "  -- will require a restart when done"
+  echo " "
+  echo " Will require a restart when complete."
+  read -n1 -s -r -p $'Press any key to continue...\n' key
 fi
 
 ### No screensaver or screen lock
@@ -56,27 +61,30 @@ gsettings set org.gnome.desktop.screensaver lock-delay 3600
 gsettings set org.gnome.desktop.screensaver idle-activation-enabled 'false'
 gsettings set org.gnome.desktop.session idle-delay 0
 
+# TODO: Ubuntu MATE desktop and color config
+mate-panel --reset --layout netbook
+sudo apt install ubuntu-mate-colours-blue
+#mate-appearance-properties --install=Ambiant-MATE-Dark-Blue
+
 ### Update the system TODO: uncomment
 #sudo apt-get update
 #sudo apt-get --yes upgrade
 
-# TODO: Ubuntu MATE desktop and color config
-
 ### Install guake terminal and git with lfs
 sudo apt-get --yes install guake git-lfs
 
-echo "deb http://archive.raspberrypi.org/debian/ buster main" | sudo tee -a /etc/apt/sources.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7FA3303E
-sudo apt-get update
-sudo apt-get --yes install raspi-config
+#echo "deb http://archive.raspberrypi.org/debian/ buster main" | sudo tee -a /etc/apt/sources.list
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7FA3303E
+#sudo apt-get update
+#sudo apt-get --yes install raspi-config
 
 # Ubuntu MATE does not have serial console enabled by default,
 # so we do not need to disable it. TODO: test this
 # If you are using regular RasPI, you may need to do this:
 #sudo raspi-config nonint do_serial 2
 
-### disable screen blanking
-sudo raspi-config nonint do_blanking 1
+### disable screen blanking TODO this don't work
+###sudo raspi-config nonint do_blanking 1
 
 ### disable splash screen TODO this don't work
 #sudo raspi-config nonint do_boot_splash 1
@@ -87,33 +95,42 @@ echo 'ATTRS{idVendor}=="0483", ATTRS{idProduct}=="374b", MODE="666"' | sudo tee 
 ### Clone repository and go in
 git clone https://github.com/RespiraWorks/Ventilator.git ventilator
 cd ventilator
-git checkout issue_1180_cmake_build_on_rpi  #TODO: remove this
-git pull #TODO remove this
+
+###############################################################################
+### TODO: Uncomment this section only when tweaking this script in a branch ###
+###############################################################################
+git checkout issue_1180_cmake_build_on_rpi
+git pull
+###############################################################################
 
 ### guake on startup and settings
 mkdir -p /home/respira/.config/autostart
 cp ./software/utils/rpi_config/user_config/autostart/* /home/respira/.config/autostart
+chmod +x /home/respira/.config/autostart/*
 dconf load /apps/guake/ < ./software/utils/rpi_config/user_config/dconf-guake-dump.txt
 
 ### Desktop shortcuts
 cp ./software/utils/rpi_config/Desktop/* /home/respira/Desktop
 chmod +x /home/respira/Desktop/*
 
-### Execute shortcuts without bitching TODO
+### Execute shortcuts without bitching TODO: is this still needed?
 #mkdir -p /home/respira/.config/libfm && cp -f software/utils/rpi_config/libfm.conf /home/respira/.config/libfm
 
 ### RW theme :)
 gsettings set org.mate.background picture-filename /home/respira/ventilator/manufacturing/images/rendering_full.jpg
 
+# Install dependencies and do initial configuration for build toolchains
 sudo ./software/gui/gui.sh install
 ./software/gui/gui.sh install_local
 sudo ./software/controller/controller.sh install
 ./software/controller/controller.sh install_local
 sudo ./software/controller/controller.sh configure
 
-echo "Installation complete. Please check that this terminated with no errors."
-echo "Upon restart, please run the 'Ventilator update' app from your desktop."
-echo " "
-read -n1 -s -r -p $'Press any key to restart the machine\n' key
+if [ -z "$VERBOSE" ]; then
+  echo "Installation complete. Please check that this terminated with no errors."
+  echo "Upon restart, please run the 'Ventilator update' app from your desktop."
+  echo " "
+  read -n1 -s -r -p $'Press any key to restart the machine\n' key
 
-#sudo shutdown -r now
+  sudo shutdown -r now
+fi

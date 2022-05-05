a326bfa30f9738447d1503a7c170707d466b9213
asmodai27 <asmodai27@wanadoo.fr>
2022-05-05 22:00:55 +0200

    fix unit tests (TODO: find a better way?)

--- software/controller/test/eeprom/eeprom_test.cpp
+++ software/controller/test/eeprom/eeprom_test.cpp
@@ -10,40 +10,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include "eeprom.h"
-
 #include "gtest/gtest.h"
-
-class TestEeprom : public I2Ceeprom {
- public:
-  TestEeprom(uint8_t address, uint8_t page_size, uint16_t size)
-      : I2Ceeprom(address, page_size, size, nullptr) {
-    for (int i = 0; i < size_; ++i) {
-      memory_[i] = 0xFF;
-    };
-  }
-
- private:
-  uint32_t address_pointer_{0};
-  uint8_t memory_[MaxMemorySize];
-  bool SendBytes(const I2C::Request &request) override {
-    address_pointer_ = reinterpret_cast<uint8_t *>(request.data)[0] << 8 |
-                       reinterpret_cast<uint8_t *>(request.data)[1];
-    for (uint32_t i = 2; i < request.size; ++i) {
-      memory_[address_pointer_++] = reinterpret_cast<uint8_t *>(request.data)[i];
-    }
-    if (request.processed != nullptr) *(request.processed) = true;
-    return true;
-  }
-
-  bool ReceiveBytes(const I2C::Request &request) override {
-    for (uint32_t i = 0; i < request.size; ++i) {
-      reinterpret_cast<uint8_t *>(request.data)[i] = memory_[address_pointer_++];
-    }
-    if (request.processed != nullptr) *(request.processed) = true;
-    return true;
-  }
-};
+#include "test_eeprom.h"
 
 static constexpr uint32_t kMemSize{512};
 uint8_t memory[kMemSize];

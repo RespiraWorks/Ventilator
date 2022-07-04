edf3cfab41be464edcbdd25c2266dffa2f4c4abf
asmodai27 <asmodai27@wanadoo.fr>
2022-07-04 23:21:13 +0200

    adding #includes in integration_test/main in order to fix new regression in pio's ldf

--- software/controller/integration_tests/main.cpp
+++ software/controller/integration_tests/main.cpp
@@ -1,3 +1,10 @@
+// Including some headers here in order for ldf to find them:
+// see https://community.platformio.org/t/ldf-not-following-environment-variables-in-6-1-0rc1/28616
+#include "actuator_base.h"
+#include "hal.h"
+#include "interface.h"
+#include "sensors.h"
+
 // Using these compile-time defines to avoid multiple directories and mains
 
 #include INTEGRATION_TEST_H

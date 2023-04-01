b447ee40fc73c3fd61adaf8b9a05e77d428f12fe
Martin Shetty <1972005+martukas@users.noreply.github.com>
2023-04-01 02:24:48 -0400

    different source command; updates #1324

--- software/gui/gui.sh
+++ software/gui/gui.sh
@@ -126,7 +126,7 @@ configure_conan() {
   pip3 install gitpython
   pip3 install conan==$CONAN_VERSION
   conan --version
-  . "${HOME}/.profile"
+  source "${HOME}/.profile"
   conan profile new --detect default
   conan profile update settings.compiler.libcxx=libstdc++11 default
 }
